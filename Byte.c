bd1adc111c2f8b456be93d91a76cf0617a0e5d2f
Madelief Hardeman <terra@proglab.nl>
2025-02-04 15:39:33 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -26,7 +26,7 @@ void print_byte(int number);
             { 
                 bit[counter] = 1;
                 number = remainder; //127 
-                counter++
+                counter++;
             } 
         
     } 

d88ebb39851099f7fde7b2eac5ad3d3b1529b131
Madelief Hardeman <terra@proglab.nl>
2025-02-04 15:01:44 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -19,6 +19,6 @@ void print_byte(int number);
     int bit3 = number - 32;
     int bit4 = number - 16;
     int bit5 = number - 8;
-    int bit6 = number - 4;
-    int bit7 = number - 2;
+    int bit6 = 0;
+    int bit7 = 0;
     int bit8 = 0

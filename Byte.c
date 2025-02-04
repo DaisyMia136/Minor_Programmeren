ee24804228ad4ab879c0e17bc3b733bf6a58b0e6
Madelief Hardeman <terra@proglab.nl>
2025-02-04 15:39:37 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -16,8 +16,8 @@ int main(void)
 void print_byte(int number); 
 { 
     // initiate bit 
-    string bit = 00000000
-    counter = 1
+    string bit = 00000000;
+    counter = 1;
     for (i = 1; 1 > 128; i * 2)
     {
         

42e04a36397629decfb88c7ebdaf223247051d7b
Madelief Hardeman <terra@proglab.nl>
2025-02-04 14:47:42 +0100

    Update Byte.c

---
+++ Byte.c
@@ -0,0 +1,55 @@
+#include <stdio.h> 
+
+// functions prototypes
+void print_byte(int number); 
+int devide_bits(number)
+
+// main script
+int main(void)
+{
+   int value_bits = [128, 64, 32]
+   printf("%i\n", value_bits)
+   void print_byte(int number); 
+    
+}
+
+// functions
+void print_byte(int number); 
+{
+    number = 129; 
+    int bit1 = number - 128;
+    int bit2 = number - 64;
+    int bit3 = number - 32;
+    int bit4 = number - 16;
+    int bit5 = number - 8;
+    int bit6 = number - 4;
+    int bit7 = number - 2;
+    int bit8 = number - 1;
+    
+    
+    if (bit1 >= 0)
+    { 
+      bit1 = 1;
+      number = number - 128;
+    } 
+    else 
+    { 
+        bit1 = 0;
+    }
+    
+    
+}
+
+int number devide_bits(number)
+{ 
+    number = 129; 
+    int bit1 = number - 128;
+    int bit2 = number - 64;
+    int bit3 = number - 32;
+    int bit4 = number - 16;
+    int bit5 = number - 8;
+    int bit6 = number - 4;
+    int bit7 = number - 2;
+    int bit8 = number - 1;
+} 
+

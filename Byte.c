e9de034c602482f1976bd4f1b077c1f754dc6e40
Madelief Hardeman <terra@proglab.nl>
2025-02-04 15:14:18 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -18,7 +18,7 @@ void print_byte(int number);
     for (i = 1; 1 > 128; i * 2)
     {
         int remainder = number - i; // remainder = 255 - 128 
-        if remainder is 
+        if remainder 
     
     
 // initiate all bits as 0    

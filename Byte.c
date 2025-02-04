30b49454b35347fc23599f76a913d122fc83fc00
Madelief Hardeman <terra@proglab.nl>
2025-02-04 15:11:11 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -6,7 +6,7 @@
 // main script
 int main(void)
 {
-    int bit_values[] = {128, 64, 32, 16, 8, 4, 2, 1}; 
+    //int bit_values[] = {128, 64, 32, 16, 8, 4, 2, 1}; 
     printf("%i\n", bit_values[1]);
     void print_byte(int number); 
     

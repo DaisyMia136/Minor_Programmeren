359620b584066e4818efd49d2c1985537d8357b8
Madelief Hardeman <terra@proglab.nl>
2025-02-04 11:11:11 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,5 +4,5 @@
 int main(void)
 {
     string answer = get_string("What's your name? "); //ask for persons name using get_string and save it in answer as a string
-    printf("Hello, %s\n", answer); // combine "Hello" with inputted name 
+    printf("Hello, %s\n", answer); // combine "Hello" with inputted name and print the combined string
 }

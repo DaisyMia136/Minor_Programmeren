565b547da68c87250073506b1f88c5cdcb42cd1d
Madelief Hardeman <terra@proglab.nl>
2025-02-04 16:57:05 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -17,7 +17,7 @@ void print_byte(int number)
 {
     // initiate bit 
     int bit[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    print("%d\n", bit);
+    printf("%d\n", bit);
     int counter = 0;
     for (int i = 1; 1 > 128; i = i * 2)
     {

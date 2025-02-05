669c89c15272df3e29e2ce3fe173fb1be4207779
Madelief Hardeman <terra@proglab.nl>
2025-02-05 08:35:25 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -6,59 +6,46 @@ void print_byte(int number);
 // main script
 int main(void)
 {
-    int bit[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    //printf("%d\n", bit[1]);
-    int counter = 0;
-    int number = 255;
-    for (int i = 128; i > 0; i = i / 2 )
-    {
-        printf("Counter is %i\n", counter);
-        printf("Number is %i\n", number);
-        printf("i is %i\n", i);
-        int remainder = number - i; // remainder = 255 - 128 
-        printf("remainder is %i\n", remainder);
-        if (remainder >= 0) 
-            { 
-                bit[counter] = 1;
-                number = remainder; //127 
-                //printf("%i\n", bit);
-            } 
-        counter++;
-        
-    } 
-    //int bit_values[] = {128, 64, 32, 16, 8, 4, 2, 1}; 
-    //int number = 65;
-    //void print_byte(int number); 
+    
+    //printf("%d\n", bit[0]);
+
+    int number = 15;
+    
+    void print_byte(int number); 
     
 }
 
 
 void print_byte(int number) 
-{
-    // initiate bit 
+{ 
     int bit[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    printf("%d\n", bit[1]);
     int counter = 0;
-    for (int i = 1; 1 > 128; i = i * 2)
-    {
-        
-        int remainder = number - i; // remainder = 255 - 128 
-        if (remainder >= 0) 
+    for (int i = 128; i >= 1; i = i / 2 )
+        {
+            //printf("Counter is %i\n", counter);
+            //printf("Number is %i\n", number);
+            //printf("i is %i\n", i);
+            int remainder = number - i; // remainder = 255 - 128 
+            //printf("remainder is %i\n", remainder);
+            if (remainder >= 0) 
+                { 
+                    bit[counter] = 1;
+                    //printf("Current bit is %d\n",bit[counter]);
+                    number = remainder; //127 
+                    printf("%d", bit[counter]);
+                } 
+            else 
             { 
-                bit[counter] = 1;
-                number = remainder; //127 
-                counter++;
-                //printf("%i\n", bit);
+                printf("%d", bit[counter]);
             } 
-        
-    } 
-} 
-// // initiate all bits as 0    
-//     int bit1 = 0;
-//     int bit2 = 0;
-//     int bit3 = 0;
-//     int bit4 = 0;
-//     int bit5 = 0;
-//     int bit6 = 0;
-//     int bit7 = 0;
-//     int bit8 = 0;
+            counter++;
+            
+        } 
+}
+
+
+
+
+
+
+

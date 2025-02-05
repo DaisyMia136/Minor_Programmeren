c01ddfa15e1c1edee12eab76c62b97692fea0b7b
Madelief Hardeman <terra@proglab.nl>
2025-02-05 09:49:51 +0100

    Update Byte.c

--- Byte.c
+++ Byte.c
@@ -8,31 +8,6 @@ int main(void)
 {
     int number = 12;
     print_byte(int number); 
-    int bit[] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int counter = 0;
-    if (number > 255 || number < 0)
-    { 
-        printf("Please input a number between 0 and 255\n");
-    } 
-    for (int i = 128; i >= 1; i = i / 2 )
-    {
-        //printf("Counter is %i\n", counter);
-        //printf("Number is %i\n", number);
-        //printf("i is %i\n", i);
-        int remainder = number - i;  
-        //printf("remainder is %i\n", remainder);
-        if (remainder >= 0) 
-        { 
-            bit[counter] = 1;
-            //printf("Current bit is %d\n",bit[counter]);
-            number = remainder; //127                     
-            printf("%d", bit[counter]);
-        } 
-        else 
-        { 
-            printf("%d", bit[counter]);
-        } 
-        counter++;
             
     } 
 }

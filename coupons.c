1f47c4d5d58ab2ef358fac0b0f112047dd3b003b
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:52:05 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -50,7 +50,7 @@ float bereken_coupon1(int aantal_stuks, float prijs)
 // coupon 2 > tweede halve prijs
 float bereken_coupon2(int aantal_stuks, float prijs)
 { 
-    float fullprice_products = aantal_stuks * prijs; 
+    float fullprice_products = aantal_stuks / 2 * prijs; 
     float fullprice_products = 
     
     int multiplier = aantal_stuks / 2; // how many sets of three are in the number of products  

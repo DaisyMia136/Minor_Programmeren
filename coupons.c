c8e9b37b52d3b63740ee9c016c543cf04f765eee
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:05:03 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -22,7 +22,7 @@ int main(void)
    
    // print de beste prijs
    printf("Met coupon 1 kost dat %f\n", price_coupon_1);
-   printf("Met coupon 2 kost dat %f", price_coupon_2);
+   printf("Met coupon 2 kost dat %f\n", price_coupon_2);
    
 }
 

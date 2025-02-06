20e0cafcb19f4b7215a212f90138e64fcf4eec39
Madelief Hardeman <terra@proglab.nl>
2025-02-06 12:50:32 +0100

    Update rechthoeken.c

--- rechthoeken.c
+++ rechthoeken.c
@@ -11,7 +11,7 @@ int main(void)
     recht1_lengte = get_int("Lengte 1: ");
     recht1_breedte = get_int"Breedte 1:" );
     recht2_lengte = get_int("Lengte 2: ");
-    recht2_Breedte = get_int("Breedte 2: ");
+    recht2_breedte = get_int("Breedte 2: ");
     
     calculation = 
     

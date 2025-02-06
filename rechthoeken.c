7d038e16513ed61996fa182e0f87bc536e7713bd
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:18:33 +0100

    Update rechthoeken.c

--- rechthoeken.c
+++ rechthoeken.c
@@ -44,12 +44,12 @@ int do_calc(int recht1_lengte, int recht1_breedte, int recht2_lengte, int recht2
     }
     else if (strcmp(calculation_option, "S") == 0) // bereken de som van de twee rechthoeken
     {
-        int answer = opp_1 + opp_2;
+        answer = opp_1 + opp_2;
         return answer;
     }
     else if (strcmp(calculation_option, "V") == 0) // bereken het verschil van de twee rechthoeken
     {
-        int answer = opp_1 - opp_2;
+        answer = opp_1 - opp_2;
         return answer;
     }
 

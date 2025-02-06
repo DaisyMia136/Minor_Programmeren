e692c5122fd11bbc46bda59bc498ba55918d3c16
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:44:25 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -12,7 +12,7 @@ float calc_coup_3();
 int main(void) 
 {
    // vraag de hoeveelheid producten
-   int product_number get_int
+   int product_number = get_int("")
    
    // vraag de prijs per stuk
    

c3d3855d746d210635e9f34fb154e404c53e2e26
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:03:28 +0100

    Update rechthoeken.c

--- rechthoeken.c
+++ rechthoeken.c
@@ -29,6 +29,7 @@ int main(void)
 int do_calc(recht1_lengte, recht1_breedte, recht2_lengte, recht2_breedte, calculation_option)
 {
     int opp_1 = int recht1_lengte * int recht1_breedte; 
+    int opp_2 = int recht2_lengte * int recht2_breedte; 
     
     if (char * calculation_option == "1"  || calculation_option == "S") // bereken oppervlakte van t eerste rechthoek
     {
@@ -36,7 +37,7 @@ int do_calc(recht1_lengte, recht1_breedte, recht2_lengte, recht2_breedte, calcul
     } 
     else if (calculation_option == "2" ) // bereken oppervlakte van t tweede rechthoek
     {
-        int opp_2 = int recht2_lengte * int recht2_breedte; 
+        
     }
     else if (calculation_option == S) // bereken de som van de twee rechthoeken
     {

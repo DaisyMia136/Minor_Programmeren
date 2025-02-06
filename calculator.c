cc570ffb5c15a715354f1f5a9d0fcab2e7b01cf9
Madelief Hardeman <terra@proglab.nl>
2025-02-06 16:09:36 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -16,6 +16,7 @@ int main(void)
             int input1 = input;
             string input2 = get_string("");
             if (input2 == '+' || input2 == '-' || input2 == '/' || input2 == '*')
+            
         }
        
        

6db8d21067227a6d5f8917bc648cd66ca924b06b
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:12:01 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -50,7 +50,10 @@ float bereken_coupon2(int aantal_stuks, float prijs)
 // coupon 3 > stapel korting
 float bereken_coupon3(int aantal_stuks, float prijs)
 { 
-    if ()
+    if (aantal_stuks == 1)
+    { 
+    } 
+    else if (aantal_stuks == )
     
     for (int i = 0; i < 4; i++)
     {

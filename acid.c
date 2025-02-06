5e2bbd25c5f2b22b50019bc0536a4303883bb4d0
Madelief Hardeman <terra@proglab.nl>
2025-02-06 11:11:28 +0100

    Update acid.c

--- acid.c
+++ acid.c
@@ -1,4 +1,6 @@
 #include <cs50.h>
+#include <stdio.h>
+
 // function prototypes
 bool is_acidic(float value);
 
@@ -6,16 +8,39 @@ bool is_acidic(float value);
 int main(void)
 {
     float input = get_float("Vul het PH level in: ");
-    is_acidic(input)
-    // roep functie aan en print boodschap afhankelijk van het resultaat
+    bool acid = is_acidic(input);
+    if (input < 0 || input > 14)
+    { 
+        printf("Please input a number between 0 and 14\n");
+    } 
+    else 
+    {
+        if (acid == true)
+        { 
+            printf("This is an acid\n");
+        } 
+        else 
+        {
+            printf("This is a base\n");
+        } 
+    } 
+        // roep functie aan en print boodschap afhankelijk van het resultaat
 }
 
 // functions
 bool is_acidic(float value)
 {
+    //set defaults
+    bool acid = false;
+    
+    // decide if inputted value is acidic, basic or neurtral
     if (value < 7.0)
     { 
-        
-    
-    // beslis of dit zuur is (ofwel kleiner dan 7.0)
+      acid = true;
+    } 
+    else 
+    { 
+        acid = false;
+    } 
+    return acid;
 }

acdf409d82794b5bcaf88eacf41b4e9cf991cb3d
Madelief Hardeman <terra@proglab.nl>
2025-02-06 15:15:22 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -59,7 +59,7 @@ float bereken_coupon2(int aantal_stuks, float prijs)
     float fullprice_products = multiplier * prijs; // calculate the price of the full price products
     float halfprice_products = remainder * (prijs / 2); // calculate the price of the half price products
     float price_coupon_2 = fullprice_products + halfprice_products; 
-    printf("%i\n", multiplier);
+    printf("%f\n", fullprice_products);
     
     return price_coupon_2;
 }

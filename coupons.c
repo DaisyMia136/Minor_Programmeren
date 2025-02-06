90c23d0cd429635f228fae2435f69353e1f741f7
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:31:12 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -22,6 +22,7 @@ int main(void)
    float price_coupon_3 = bereken_coupon3(aantal_stuks, prijs);
    
    // print de beste prijs
+   
    printf("Met coupon 1 kost dat %f\n", price_coupon_1);
    printf("Met coupon 2 kost dat %f\n", price_coupon_2);
    printf("Met coupon 3 kost dat %f\n", price_coupon_3);

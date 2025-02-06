0bc8ae67849566797853946f537430ab5db783da
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:28:03 +0100

    Update rechthoeken.c

--- rechthoeken.c
+++ rechthoeken.c
@@ -19,7 +19,7 @@ int main(void)
     // vraag welke optie er uitgevoerd moet worden
     char * calculation_option = get_string("Wil je de oppervlakte van rechthoek (1) of (2), of de (S)om of het (V)erschil weten? ");
     
-    if (strcmp(calculation_option, "1") != 1 && strcmp(calculation_option, "2") != 1 && strcmp(calculation_option, "S") != 1 &&)
+    if (strcmp(calculation_option, "1") != 1 && strcmp(calculation_option, "2") != 1 && strcmp(calculation_option, "S") != 1 && strcmp(calculation_option, "V") != 1 &&)
     // Doe de berekening met de ingevoerde opties en gegevens
     int answer = do_calc(recht1_lengte, recht1_breedte, recht2_lengte, recht2_breedte, calculation_option);
     printf("%i\n", answer);

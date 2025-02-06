9760fc0f94922bc8cecd9d9ee40c88a5bc05e938
Madelief Hardeman <terra@proglab.nl>
2025-02-06 11:16:25 +0100

    Update Orakel.c

--- Orakel.c
+++ Orakel.c
@@ -7,5 +7,5 @@
 
 int main(void)
 { 
-    get("Wat is het antwoord op de grote vraag van het leven, het universum en alles daarbij? \n"
+    Univers_answer = get("Wat is het antwoord op de grote vraag van het leven, het universum en alles daarbij? \n")
 } 

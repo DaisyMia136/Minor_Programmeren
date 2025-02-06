ff90ff0e1b76b77d9c00998dd4621c1637cfe36a
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:20:57 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -50,6 +50,8 @@ float bereken_coupon2(int aantal_stuks, float prijs)
 // coupon 3 > stapel korting
 float bereken_coupon3(int aantal_stuks, float prijs)
 { 
+    // set defaults
+    
     if (aantal_stuks == 1)
     { 
         float discount = prijs / 20;

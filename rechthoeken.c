9e4e42af51b9b2878fae255f21fe71fdb6cec289
Madelief Hardeman <terra@proglab.nl>
2025-02-06 12:59:02 +0100

    Update rechthoeken.c

--- rechthoeken.c
+++ rechthoeken.c
@@ -28,7 +28,7 @@ int main(void)
 // functions 
 do_calc(recht1_lengte, recht1_breedte, recht2_lengte, recht2_breedte, calculation_option)
 {
-    if ( calculation_option == 1)
+    if (char * calculation_option == 1)
     {
         
     } 

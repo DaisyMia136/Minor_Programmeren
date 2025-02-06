8b02336d33917a2f0544b4a8c2cfc880ca5d1083
Madelief Hardeman <terra@proglab.nl>
2025-02-06 16:00:45 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -57,9 +57,8 @@ int main(void)
    }
    else if (price_coupon_3 == price_coupon_1)
    {
-       printf("Met coupon 2 kost dat %.2f\n", price_coupon_2);
+       printf("Met coupon 3 kost dat %.2f\n", price_coupon_3);
    } 
-   }
    
    else if (price_coupon_2 < price_coupon_1 && price_coupon_2 < price_coupon_3)
    { 

e43ae5acc8035b6735f811e882307fcadb746041
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:42:09 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -11,7 +11,7 @@ int main(void)
    
    // vraag de prijs per stuk
    
-   // bereken elke prijs met de verschillende coupons
+   // bereken elke prijs met de verschillende couponsns
    
    
    // print de beste prijs

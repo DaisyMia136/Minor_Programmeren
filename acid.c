fa7f62428e4f65b18f7ae81cb7f3f2612b58fce2
Madelief Hardeman <terra@proglab.nl>
2025-02-06 10:42:55 +0100

    Update acid.c

---
+++ acid.c
@@ -0,0 +1,14 @@
+#include 
+bool is_acidic(float value);
+
+int main(void)
+{
+    float input = get_float("Vul het PH level in: ");
+    
+    // roep functie aan en print boodschap afhankelijk van het resultaat
+}
+
+bool is_acidic(float value)
+{
+    // beslis of dit zuur is (ofwel kleiner dan 7.0)
+}

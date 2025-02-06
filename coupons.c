4269fca64c0fe660fd7fa392aaad403099ceb513
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:19:41 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -53,17 +53,17 @@ float bereken_coupon3(int aantal_stuks, float prijs)
     if (aantal_stuks == 1)
     { 
         float discount = prijs / 20;
-        price_coupon_2 = prijs - discount;
+        price_coupon_3 = prijs - discount;
     } 
     else if (aantal_stuks == 2)
     {
         float discount = prijs / 10;
-        price_coupon_2 = prijs - discount;
+        price_coupon_3 = prijs - discount;
     }
     else if (aantal_stuks == 3)
     {
         float discount = prijs / 5;
-        price_coupon_2 = prijs - discount;
+        price_coupon_3 = prijs - discount;
     } 
     else 
     {

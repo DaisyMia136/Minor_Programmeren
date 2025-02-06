4f9ae2ae77cc9b0ac5d1d94418a0770d3ead0bb8
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:54:48 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -27,7 +27,7 @@ int main(void)
 // coupon 1 > elke derde is gratis
 float calc_coup_1(int product_number, float product_cost) 
 {
-    int multiplier = product_number / 3; // how many sets of three 
+    int multiplier = product_number / 3; // how many sets of three are in the number of products
     float discount = multiplier * product_cost;
     float price_coupon_1 = product_number * product_cost - discount;
     return price_coupon_1;

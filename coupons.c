9186c6fe97890f40ebed2c2949bcd386ce4b0791
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:24:38 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -43,11 +43,11 @@ float bereken_coupon2(int aantal_stuks, float prijs)
   int multiplier = aantal_stuks / 2; // how many sets of three are in the number of products  
   if (multiplier == 0)
   { 
-      price_coupon_2 = prijs; 
+      float price_coupon_2 = prijs; 
   } 
   float fullprice_products = multiplier * prijs; //calculate the price of the full price products
   float halfprice_products = multiplier * (prijs / 2); //calculate the price of the half price products
-  float price_coupon_2 = fullprice_products + halfprice_products; 
+  price_coupon_2 = fullprice_products + halfprice_products; 
   return price_coupon_2;
 }
 

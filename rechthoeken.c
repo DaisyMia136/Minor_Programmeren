63bef541dbbd2ec4aed49c3d44e38923844d209a
Madelief Hardeman <terra@proglab.nl>
2025-02-06 13:04:18 +0100

    Update rechthoeken.c

--- rechthoeken.c
+++ rechthoeken.c
@@ -45,7 +45,7 @@ int do_calc(recht1_lengte, recht1_breedte, recht2_lengte, recht2_breedte, calcul
     }
     else if (calculation_option == V) // bereken het verschil van de twee rechthoeken
     {
-        
+        opp_1 - opp_2
     }
 
 }

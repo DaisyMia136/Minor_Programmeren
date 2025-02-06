b97737c8fb86911a108eeb3c45a71399321977b0
Madelief Hardeman <terra@proglab.nl>
2025-02-06 14:37:11 +0100

    Update coupons.c

--- coupons.c
+++ coupons.c
@@ -23,6 +23,7 @@ int main(void)
    
    // print de beste prijs
    float lowest_price = min(price_coupon_1, price_coupon_2, price_coupon_3); 
+   if 
    printf("Met coupon 1 kost dat %f\n", price_coupon_1);
    printf("Met coupon 2 kost dat %f\n", price_coupon_2);
    printf("Met coupon 3 kost dat %f\n", price_coupon_3);

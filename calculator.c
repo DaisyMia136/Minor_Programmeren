597b1fefab47b56bb6d47c7f81e9c24fc33e2272
Madelief Hardeman <terra@proglab.nl>
2025-02-06 16:08:52 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -14,6 +14,7 @@ int main(void)
         if (input == 0 || input == 1 || input == 2 || input == 3 || input == 4 || input == 5 || input == 6 || input == 7 || input == 8 || input == 9)
         {
             int input1 = input;
+            string input2 = get_string("");
         }
         else if (input == + || input == - || input == / || input == *)
         {

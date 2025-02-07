42c1e08fe1a6274f2de55b4134ca9296bc70bdc6
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:25:49 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -14,7 +14,7 @@ int main(void)
     int i = 1;
     while (i == 1)
     { 
-        if (
+        if (input1 >
             input1 == 0 || input1 == 1 || input1 == 2 || input1 == 3 || input1 == 4 || input1 == 5 || input1 == 6 || input1 == 7 || input1 == 8 || input1 == 9)
         {
             // ask for input2

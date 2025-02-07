3d0111f51fdf42ddd63c519b64aaa9caadd863f4
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:00:06 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -22,7 +22,20 @@ int main(void)
                     if (input == 0 || input == 1 || input == 2 || input == 3 || input == 4 || input == 5 || input == 6 || input == 7 || input == 8 || input == 9)
                         {
                             // do calc
-                        
+                        if (input2 == '+')  
+            {
+                
+            }
+        else if (input2 == '-') 
+            {
+            }
+        else if (input2 == '/') 
+            {
+                
+            }
+        else if (input2 == '*')
+            { 
+            } 
                             
                         }
                     else

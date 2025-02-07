b85a95dcb8cfe0e5b2b32a314eadfd52c000d405
Madelief Hardeman <terra@proglab.nl>
2025-02-07 14:29:30 +0100

    Update mario.c

--- mario.c
+++ mario.c
@@ -34,7 +34,7 @@ void build_pyramid(int input)
     for(int i = 0; i < input; i++)
     {
         int counter_hash = 1;
-        int count_space = 
+        int count_space = input;
         int number_hash = input - counter; 
         int number_space = counter;
     }

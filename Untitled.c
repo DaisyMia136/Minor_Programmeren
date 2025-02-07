fb415b77001424af764c463b5e843f7694f0724c
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:21:23 +0100

    Rename Untitled to Untitled.c

---
+++ Untitled.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+// function prototypes
+
+//main script
+int main(void)
+{
+    // set defaults
+    string input1 = get_string("");
+    printf("%s", input1);
+    
+} 

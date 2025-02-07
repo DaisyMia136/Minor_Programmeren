90f14725fa19b5f75e1c5534e1486bbe2fc6d86b
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:03:33 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -21,23 +21,23 @@ int main(void)
                 int input3 = get_int("");
                     if (input == 0 || input == 1 || input == 2 || input == 3 || input == 4 || input == 5 || input == 6 || input == 7 || input == 8 || input == 9)
                         {
-                        // do calc
-                        if (input2 == '+')  
-                        {
-                            output = input1 + input3;
-                        }
-                        else if (input2 == '-') 
-                        {
-                            output = input1 - input3;
-                        }
-                        else if (input2 == '/') 
-                        {
-                            output = input1 / input3;                   
-                        }
-                        else if (input2 == '*')
-                        {
-                            output = input1 * input3;
-                        }
+                            // do calc
+                            if (input2 == '+')  
+                            {
+                                output = input1 + input3;
+                            }
+                            else if (input2 == '-') 
+                            {
+                                output = input1 - input3;
+                            }
+                            else if (input2 == '/') 
+                            {
+                                output = input1 / input3;                   
+                            }
+                            else if (input2 == '*')
+                            {
+                                output = input1 * input3;
+                            }
                             
                         }
                     else

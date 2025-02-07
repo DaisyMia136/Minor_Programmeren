4c3e5e07e816e4b1dd8132112af9baced8e330b7
Madelief Hardeman <terra@proglab.nl>
2025-02-07 12:23:02 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -19,6 +19,8 @@ int main(void)
             { 
                 // ask for intput3
                 int intput3 = get_int("");
+                        if (input == 0 || input == 1 || input == 2 || input == 3 || input == 4 || input == 5 || input == 6 || input == 7 || input == 8 || input == 9)
+
             } 
             
         } 

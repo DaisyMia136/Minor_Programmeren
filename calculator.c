35501048498ee18501802c83868388a4a00f37f5
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:01:18 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -24,7 +24,7 @@ int main(void)
                         // do calc
                         if (input2 == '+')  
                         {
-                
+                        output = 
                         }
                         else if (input2 == '-') 
                         {

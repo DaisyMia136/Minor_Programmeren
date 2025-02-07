88fc0e91dd3f99e756f114118f6551d0ccc8b62c
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:03:21 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -32,11 +32,11 @@ int main(void)
                         }
                         else if (input2 == '/') 
                         {
-                            output = input1 / input3                    
+                            output = input1 / input3;                   
                         }
                         else if (input2 == '*')
                         {
-                            
+                            output = input1 * input3;
                         }
                             
                         }

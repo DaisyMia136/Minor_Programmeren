5884969955d780ca955340e98f02d23a75dd272e
Madelief Hardeman <terra@proglab.nl>
2025-02-07 12:58:10 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -26,6 +26,9 @@ int main(void)
                             
                         }
             } 
+            else 
+                { 
+                    input
             
         } 
         else 

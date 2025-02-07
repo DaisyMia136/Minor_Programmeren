1d29646345155052c2b2c7ace91606c250fa21c6
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:50:50 +0100

    Update repeat.c

--- repeat.c
+++ repeat.c
@@ -10,7 +10,7 @@ int main(void)
     string input_string = get_string("");
     int input_number = get_int("");
     
-    for (int i = intput_number; i > 0; i)
+    for (int i = intput_number; i > 0; i )
 }
 
 

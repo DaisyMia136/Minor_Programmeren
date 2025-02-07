071531e95e88b925d09d61a38839332ce0d3ce4e
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:04:23 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -22,6 +22,7 @@ int main(void)
                     if (input == 0 || input == 1 || input == 2 || input == 3 || input == 4 || input == 5 || input == 6 || input == 7 || input == 8 || input == 9)
                         {
                             // do calc
+                            int output = 0;
                             if (input2 == '+')  
                             {
                                 output = input1 + input3;

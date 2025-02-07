66a15b279dd8780cd463ed99a8d14c15e7cc7525
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:02:49 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -60,7 +60,7 @@ int main(void)
         if (strcmp(input2, "+") == 0 || strcmp(input2, "-")  == 0 || strcmp(input2, "/")  == 0 || strcmp(input2, "*") == 0)
         { 
             // ask for intput3
-            int intput3 = get_int("")
+            int input3 = get_int("")
         } 
         else 
         { 

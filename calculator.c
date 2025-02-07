bb6abf952f16ba4734af01a7d9de8c5504b352ca
Madelief Hardeman <terra@proglab.nl>
2025-02-07 12:59:27 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -22,7 +22,7 @@ int main(void)
                     if (input == 0 || input == 1 || input == 2 || input == 3 || input == 4 || input == 5 || input == 6 || input == 7 || input == 8 || input == 9)
                         {
                             // do calc
-                            
+                        
                             
                         }
                     else

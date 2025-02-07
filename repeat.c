34b5c3ba0993812a1c99fb32b635f00ad91c0899
Madelief Hardeman <terra@proglab.nl>
2025-02-07 13:52:33 +0100

    Update repeat.c

--- repeat.c
+++ repeat.c
@@ -10,7 +10,7 @@ int main(void)
     string input_string = get_string("");
     int input_number = get_int("");
     
-    for (int i = 0; i > input_number; i++)
+    for (int i = 0; i < input_number; i++)
     { 
         printf("%s\n", input_string);
     } 

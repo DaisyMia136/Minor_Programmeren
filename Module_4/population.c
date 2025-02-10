47c3fb2cc84be727a4bd900dbf5bbb0cb8042653
Madelief Hardeman <terra@proglab.nl>
2025-02-10 13:07:05 +0100

    Update Module_4/population.c

--- Module_4/population.c
+++ Module_4/population.c
@@ -45,4 +45,6 @@ int llama_calc(int start_population, int end_population)
         start_population = start_population + llama_babies - llama_deaths; 
         printf("Current population is: %i", start_population);
     } 
+    years = 0;
+    return years
 }

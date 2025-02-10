49f86efc7c2897b0b600e0e6a5d2ec18c7f53b00
Madelief Hardeman <terra@proglab.nl>
2025-02-10 10:28:49 +0100

    Update driehoek.c

--- driehoek.c
+++ driehoek.c
@@ -25,7 +25,7 @@ void print_triangle(int heigth)
     int triangle_length = heigth * 2;
     printf("%i", triangle_length);
     // print each row
-    for(int row = 0; row > heigth; row++)
+    for(int row = 0; row < heigth; row++)
     {
         int outer_spaces = heigth - 1;
         printf("%i", outer_spaces);

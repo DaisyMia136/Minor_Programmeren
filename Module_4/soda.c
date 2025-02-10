3ed18b12501cca17cc412477448e8ae562b0760c
Madelief Hardeman <terra@proglab.nl>
2025-02-10 13:38:41 +0100

    Update Module_4/soda.c

--- Module_4/soda.c
+++ Module_4/soda.c
@@ -26,7 +26,8 @@ void calc_owed()
     while (owed >= 0)
     { 
         int coin = get_int("Insert coin: ");
-    if(coin == 25 || coin == 10 || coin == 5)
+        if(coin == 25 || coin == 10 || coin == 5)
+    
         owed = owed - coin;
     
     if (owed > 0)

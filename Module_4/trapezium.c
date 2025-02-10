5cf5b065924a2aeee1f6fdd1d6d475eb97439ab1
Madelief Hardeman <terra@proglab.nl>
2025-02-10 11:03:35 +0100

    Update Module_4/trapezium.c

--- Module_4/trapezium.c
+++ Module_4/trapezium.c
@@ -12,3 +12,20 @@ int main(void)
 
 // functions 
 
+// print a number of spaces
+void print_space(int input_space)
+{
+    for(int space_counter = 0; space_counter < input_space; space_counter++)
+    {
+        printf(" ");
+    }
+}
+
+// print a number of #
+void print_hash(int input_hash)
+{
+    for(int hash_counter = 0; hash_counter < input_hash; hash_counter++)
+    {
+        printf("#");
+    }
+}

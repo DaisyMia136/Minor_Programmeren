1384f9f3f530056a37d5c8ae35920f014b4b17dc
Madelief Hardeman <terra@proglab.nl>
2025-02-10 11:22:22 +0100

    Update Module_4/trapezium.c

--- Module_4/trapezium.c
+++ Module_4/trapezium.c
@@ -35,7 +35,10 @@ void print_trapezium(height)
     // print each row
     for(int row = 0; row < height; row++)
     {
-        outer_spaces = outer_spaces - 1; 
+        outer_spaces = outer_spaces - 1; // every row one outer space less
+        print_space(outer_spaces); // print the number of spaces for each row
+        
+        
         
     }
 }

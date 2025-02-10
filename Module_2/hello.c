f08336a4f011663bd158bf4c69618103e9d1318d
Madelief Hardeman <terra@proglab.nl>
2025-02-10 10:55:35 +0100

    Rename hello.c to Module_2/hello.c

---
+++ Module_2/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string answer = get_string("What's your name? "); //ask for persons name using get_string and save it in answer as a string
+    printf("Hello, %s\n", answer); // combine "Hello" with inputted name and print the combined string
+}

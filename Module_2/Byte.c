7567a503a6e3a1dda73f154db1b5685b808c944e
Madelief Hardeman <terra@proglab.nl>
2025-02-10 12:47:41 +0100

    Rename Byte.c to Module_2/Byte.c

---
+++ Module_2/Byte.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+
+// functions prototypes
+void print_byte(int number);
+
+// main script
+int main(void)
+{
+    int number = 57; // a number between 0 and 255
+    
+    print_byte(number); 
+            
+     
+}
+
+// functions 
+void print_byte(int number) 
+{ 
+    int bit[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int counter = 0;
+    for (int i = 128; i >= 1; i = i / 2 )
+    {
+        //printf("Counter is %i\n", counter);
+        //printf("Number is %i\n", number);
+        //printf("i is %i\n", i);
+        
+        int remainder = number - i; // remainder = 255 - 128 
+        //printf("remainder is %i\n", remainder);
+        if (remainder >= 0) 
+        { 
+            bit[counter] = 1;
+            //printf("Current bit is %i\n",bit[counter]);
+            number = remainder; //127                     
+            printf("%i", bit[counter]);
+        } 
+        else 
+        { 
+            printf("%i", bit[counter]);
+        } 
+        counter++;
+            
+    } 
+}
+
+
+
+
+
+
+

b621f8618d0eb8bc19a8339231ce5fa5429dbdec
Madelief Hardeman <terra@proglab.nl>
2025-02-10 10:34:21 +0100

    Update driehoek.c

--- driehoek.c
+++ driehoek.c
@@ -36,7 +36,7 @@ void print_triangle(int heigth)
         printf("%i\n", outer_spaces);
         print_space(outer_spaces);
         //print_hash();
-        //print_space();
+        //print_space(inner_spaces);
         //print_hash();
         //print_space(outer_spaces); // misschien onnodig
     }

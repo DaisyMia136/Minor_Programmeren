1728edf0fe444ffa2ad41db82af0f0b4ef968126
Madelief Hardeman <terra@proglab.nl>
2025-02-10 11:51:32 +0100

    Update Module_4/regen.c

--- Module_4/regen.c
+++ Module_4/regen.c
@@ -17,7 +17,7 @@ int main(void)
         { 
             input = get_int("Hoevoel: ");
             aantal_dagen = aantal_dagen++;
-            totaal_regen = input + input; 
+            regen = regen + input; 
         }
     
     input_counter = aantal_dagen;

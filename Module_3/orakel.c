fb3980ecc7fbc63cf53122dfe8032631473055f1
Madelief Hardeman <terra@proglab.nl>
2025-02-10 10:56:21 +0100

    Rename orakel.c to Module_3/orakel.c

---
+++ Module_3/orakel.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// function prototypes
+bool check_answer(char * universal_answer);
+
+//main function
+int main(void)
+{
+    char * universal_answer = get_string("Wat is het antwoord op de grote vraag van het leven, het universum en alles daarbij? \n"); 
+    bool ultimate_knowlegde = check_answer(universal_answer);
+    if (ultimate_knowlegde == true)
+    {
+        printf("Ja");
+    }
+    else 
+    { 
+        printf("Nee");
+    } 
+} 
+
+
+//functions
+bool check_answer(char * universal_answer)
+{ 
+    //set defaults 
+    bool ultimate_knowlegde = false;
+    
+    // check if universal_answer is correct
+    if (strcmp(universal_answer, "42") == 0 || strcmp(universal_answer, "tweeenveertig") == 0 ||  strcmp(universal_answer, "forty two") == 0 || strcmp(universal_answer, "forty-two") == 0)  
+    { 
+        ultimate_knowlegde = true;
+    } 
+    return ultimate_knowlegde;
+}

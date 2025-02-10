e17bd5f128fa0dc76564f5ac30f8a03af551486a
Madelief Hardeman <terra@proglab.nl>
2025-02-10 10:56:36 +0100

    Rename repeat.c to Module_4/repeat.c

---
+++ Module_4/repeat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+// function prototypes
+
+// main script
+int main(void)
+{
+    string input_string = get_string("What to repeat?: ");
+    int input_number = get_int("How many times?: ");
+    
+    for (int i = 0; i < input_number; i++)
+    { 
+        printf("%s", input_string);
+    } 
+}
+
+
+// functions

3f69ee763a2df5f23e8f0a93a0b7c78afd92d909
Madelief Hardeman <terra@proglab.nl>
2025-02-11 09:11:13 +0100

    Update Module_5/caffeine.c

--- Module_5/caffeine.c
+++ Module_5/caffeine.c
@@ -44,7 +44,7 @@ int caffeine_calculator(int input_milli)
         if (input_milli < 70)
         {
             input_milli = input_milli - 70;
-            printf()
+            printf("1 espresso");
         }
         else if (input_milli < 30)
         {

62c5f253921baaedce56891ca093727403d91606
Madelief Hardeman <terra@proglab.nl>
2025-02-11 09:15:54 +0100

    Update Module_5/caffeine.c

--- Module_5/caffeine.c
+++ Module_5/caffeine.c
@@ -75,10 +75,8 @@ int caffeine_calculator(int input_milli)
         // print the number of drinks 
         printf("%i espresso", espresso_counter);
         printf("%i black tea", blacktea_counter);
-        printf("%i espresso", greentea_counter);
-        printf("%i espresso", cocoa_counter);
-        printf("%i espresso", espresso_counter);
-        
+        printf("%i green tea", greentea_counter);
+        printf("%i cocoa", cocoa_counter);
         drinks_number = drinks_number + 1;
     } 
     int drinks_number = 1;

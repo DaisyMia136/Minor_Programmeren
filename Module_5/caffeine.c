2096e6310404d7a84c9591cb7f03d513c2dfd651
Madelief Hardeman <terra@proglab.nl>
2025-02-11 09:10:40 +0100

    Update Module_5/caffeine.c

--- Module_5/caffeine.c
+++ Module_5/caffeine.c
@@ -47,7 +47,7 @@ int caffeine_calculator(int input_milli)
         }
         else if (input_milli < 30)
         {
-            
+            input_milli = input_milli - 30;
         }
         else if (input_milli < 20)
         {

70bb7376a6fa09dbaffdb390ca8eed5831f8f48e
Madelief Hardeman <terra@proglab.nl>
2025-02-13 10:35:36 +0100

    Update Module_6/tennis.c

--- Module_6/tennis.c
+++ Module_6/tennis.c
@@ -26,7 +26,7 @@ int main(void)
         } 
         else 
         { 
-            printf("Player ... wint! \n")
+            printf("Player 2 wint! \n")
         } 
         // ask player 2 for input
         string word_2 = ask_word(2);

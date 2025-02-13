5155fa9cc5ed09098f801555c7a84ecbca041118
Madelief Hardeman <terra@proglab.nl>
2025-02-13 10:19:23 +0100

    Update Module_6/tennis.c

--- Module_6/tennis.c
+++ Module_6/tennis.c
@@ -12,7 +12,7 @@ int main(void)
     
     do 
     { 
-        word = ask_word(1);
+        string word = ask_word(1);
     // while eerste letter input2 == laatste letter input1
         // ask input player 1
         // var =  last letter

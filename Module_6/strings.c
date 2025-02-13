04e220e365627ff5145551804193b415d032a527
Madelief Hardeman <terra@proglab.nl>
2025-02-13 15:44:04 +0100

    Update Module_6/strings.c

--- Module_6/strings.c
+++ Module_6/strings.c
@@ -55,14 +55,15 @@ void bob(string input)
     for(int i = 0; i < lengte; i++)
     {
         
-        if (i%2 == 0 || i == 0)
-        { 
-            printf("%c", tolower(input[i]);
-        }
+       
         else if (i%2 != 0)
         {
             printf("%c", toupper(input[i]));
         } 
+        else if (i%2 == 0 || i == 0)
+        { 
+            printf("%c", tolower(input[i]));
+        }
 
     } 
 }

ada6704f45363584cbfda30478b64540e4f6a92a
Madelief Hardeman <terra@proglab.nl>
2025-02-13 10:34:32 +0100

    Update Module_6/tennis.c

--- Module_6/tennis.c
+++ Module_6/tennis.c
@@ -21,8 +21,8 @@ int main(void)
             string word_1 = ask_word(1);
         
             // last letter word player 1
-        last_letter = word_1[strlen(word_1) - 1];
-        printf("%c\n", last_letter);
+            last_letter = word_1[strlen(word_1) - 1];
+            printf("%c\n", last_letter);
         
         // ask player 2 for input
         string word_2 = ask_word(2);

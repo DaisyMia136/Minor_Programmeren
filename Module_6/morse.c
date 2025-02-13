8846733c6b604226d29e0ad81fedccc1163f40c8
Madelief Hardeman <terra@proglab.nl>
2025-02-13 13:24:54 +0100

    Update Module_6/morse.c

--- Module_6/morse.c
+++ Module_6/morse.c
@@ -32,7 +32,7 @@ void morse(string input)
     for(int letters = 0; letters < input_length; letters++)
     { 
         // initialise variables
-        char character;
+        char character[3];
         
         // for every three signals make a variable character containing the three signals as one string
         for (int signal = 0; signal < 3; signal++)

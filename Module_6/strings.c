47181094924615c1432ac2d5410ee70a55954303
Madelief Hardeman <terra@proglab.nl>
2025-02-13 15:17:19 +0100

    Update Module_6/strings.c

--- Module_6/strings.c
+++ Module_6/strings.c
@@ -43,7 +43,7 @@ void eek(string input)
     int e_counter = 0;
     for(int i = 0; i < lengte; i++)
     {
-        string character = input[i];
+        printf("%input[i];
         if(strcasecmp(character, "e") == 0)
         {
             e_counter++;

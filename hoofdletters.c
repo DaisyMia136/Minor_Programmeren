8b7330ef59d0506e465e9710c5c65e211bf1ceff
Madelief Hardeman <terra@proglab.nl>
2025-02-13 14:32:54 +0100

    Update hoofdletters.c

---
+++ hoofdletters.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+// function prototypes
+int count_upper(string text);
+
+// main script
+int main(void)
+{
+   string text = get_string("Tekst: ");
+   
+   int upper_counter = count_upper(text);
+   
+   if (upper_counter == 1)
+   { 
+       printf("1 woord met een hoofdletter");
+   } 
+   else 
+   { 
+       printf("%i woorden met een hoofdletter", upper_counter);
+   } 
+   
+}
+
+// functions 
+int count_upper(string text)
+{
+    int length_text = strlen(text);
+    //printf("%i\n", length_text);
+    
+    int hoofdletter_counter = 0;
+
+    
+    if (isupper(text[0]) == 1)
+    { 
+        hoofdletter_counter = 1;
+    } 
+    
+    
+    for(int letter = 0; letter < length_text; letter++)
+    {
+        char character = text[letter];
+        //printf("%c\n", character);
+        if (isblank(character) == 1)
+        { 
+            char first_letter = text[letter + 1];
+            //printf("%c", first_letter);
+            if (isupper(first_letter) == 1)
+            {
+                hoofdletter_counter++;
+            }
+        } 
+        
+    } 
+    return hoofdletter_counter;
+}

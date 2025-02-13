5ee0c5b0fbed9e433572eaf2ca9b0fb8881adb5a
Madelief Hardeman <terra@proglab.nl>
2025-02-13 10:26:12 +0100

    Update Module_6/tennis.c

--- Module_6/tennis.c
+++ Module_6/tennis.c
@@ -23,7 +23,7 @@ int main(void)
         string word_2 = ask_word(2);
         
         // first letter word player 2
-        char first_letter = word_1[word_2[0]];
+        char first_letter = word_2[0];
         
     } 
     while (false);

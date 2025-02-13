f4183dce7b658b53c8c5129411e148a207898c1f
Madelief Hardeman <terra@proglab.nl>
2025-02-13 14:33:48 +0100

    Update Module_6/hoofdletters.c

--- Module_6/hoofdletters.c
+++ Module_6/hoofdletters.c
@@ -54,6 +54,8 @@ int count_upper(string text)
             if (isupper(first_letter) == 1)
             {
                 hoofdletter_counter++;
+                   printf("%i", hoofdletter_counter);
+
             }
         } 
         

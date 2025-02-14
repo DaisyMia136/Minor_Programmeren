f6b279584ef02542c31625496c51b03ad0665acb
Madelief Hardeman <terra@proglab.nl>
2025-02-14 15:04:13 +0100

    Update Module_4/goldback.c

--- Module_4/goldback.c
+++ Module_4/goldback.c
@@ -41,7 +41,7 @@ void goldbach_checker(int input)
 { 
     for(int even_numbers = 2; even_numbers <= input; even_numbers++;)
     {   
-        for(int summer = 1; summer >- even_numbers)
+        for(int summer = 1; summer >= even_numbers)
     } 
 } 
 

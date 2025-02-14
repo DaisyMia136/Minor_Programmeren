4c6f51a927cb245c2a2d0b553a8d643d9c219b13
Madelief Hardeman <terra@proglab.nl>
2025-02-14 15:11:37 +0100

    Update Module_4/goldback.c

--- Module_4/goldback.c
+++ Module_4/goldback.c
@@ -56,7 +56,8 @@ void goldbach_checker(int input)
                     //check if summer_1 en summer_2 priem getallen zijn
                     bool priem_1 = is_priem(summer_1);
                     bool priem_2 = is_priem(summer_2);
-                    if 
+                    
+                    if(priem_1) 
             }
         }
         

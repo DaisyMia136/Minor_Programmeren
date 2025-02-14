2eaed19b1f02ba6aba0f5fc7671df089c22de0fc
Madelief Hardeman <terra@proglab.nl>
2025-02-14 15:22:36 +0100

    Rename Module_4/goldback.c to Module_4/goldbach.c

---
+++ Module_4/goldbach.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+// function prototypes
+bool is_priem(int summer);
+void goldbach_checker(int input); 
+
+// main script
+int main(void)
+{
+    goldbach_checker(50);    
+}
+
+// functions 
+bool is_priem(int summer)
+{
+    bool priem = false;
+    int priem_counter = 0;
+    for(int deler = 2; deler < summer; deler++)
+    {
+        int rest = summer % deler;
+        if(rest != 0)
+        { 
+            priem_counter++;  
+        }
+    } 
+    if(priem_counter == (summer - 2))
+    {
+        priem = true;
+    } 
+    return priem;
+} 
+
+void goldbach_checker(int input)
+{ 
+    // initial variables
+    bool priem_1 = false; 
+    bool priem_2 = false;
+    int sum = 0;
+    
+    for(int even_numbers = 2; even_numbers <= input; even_numbers += 2)
+    {   
+        //printf("%i\n", even_numbers);
+        for(int summer_1 = 1; summer_1 <= even_numbers; summer_1++)
+        { 
+            //summer_1 = 1;    
+            for(int summer_2 = 1; summer_2 <= even_numbers; summer_2++)
+            { 
+                //printf("%i + %i", summer_1, summer_2);
+                sum = summer_1 + summer_2;
+                if(sum == even_numbers)
+                { 
+                    //check if summer_1 en summer_2 priem getallen zijn
+                    priem_1 = is_priem(summer_1);
+                    priem_2 = is_priem(summer_2);
+                    //printf("%i %i", summer_1, summer_2);
+                    
+                    if(priem_1 != 0 && priem_2 != 0)
+                    { 
+                        printf("%i = %i + %i\n", even_numbers, summer_1,summer_2);
+                    }
+                }
+                        
+            }
+        }
+        
+    } 
+} 
+
+

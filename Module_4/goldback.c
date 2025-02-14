46bad46c927982d2dc33898c952967536372a04a
Madelief Hardeman <terra@proglab.nl>
2025-02-14 15:14:13 +0100

    Update Module_4/goldback.c

--- Module_4/goldback.c
+++ Module_4/goldback.c
@@ -55,7 +55,7 @@ void goldbach_checker(int input)
                     
                     if(priem_1 == true || priem_2 == true)
                     { 
-                        prinft("%i = %i + %i", even_numbers, summer_1,summer_2);
+                        printf("%i = %i + %i", even_numbers, summer_1,summer_2);
                     }
                         
             }

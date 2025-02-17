aa28a4253812e20edd52bd2768496308d3c215ca
Madelief Hardeman <terra@proglab.nl>
2025-02-17 13:36:30 +0100

    Update Module_7/scrabble.c

--- Module_7/scrabble.c
+++ Module_7/scrabble.c
@@ -52,7 +52,7 @@ calc_scrabble_points(string word)
         { 
             points =+ 3;
         }
-        else if (word[letter] == 'f' || word[letter] == 'h' || word[letter] == 'v' || )
+        else if (word[letter] == 'f' || word[letter] == 'h' || word[letter] == 'v' || word[letter] == 'w' || )
     }
 }
 

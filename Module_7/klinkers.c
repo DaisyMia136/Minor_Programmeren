b6d044e7117087ba21620515679fcd053c85d85e
Madelief Hardeman <terra@proglab.nl>
2025-02-17 09:32:03 +0100

    Update Module_7/klinkers.c

--- Module_7/klinkers.c
+++ Module_7/klinkers.c
@@ -26,7 +26,10 @@ int vowel_counter(string woord)
     
     for(letter=0; letter <= word_length; letter++)
     {
-        if(woord[letter] == 'a' || woord[letter] == 'e' || woord[letter] == 'u' || woord[letter] == 'a' ||
+        if(woord[letter] == 'a' || woord[letter] == 'e' || woord[letter] == 'u' || woord[letter] == 'i' || woord[letter] == 'o')
+        {
+            
+        }
     }
     
 }

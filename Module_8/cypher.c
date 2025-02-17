33d98902699d2e7983a2635f5b51887c09a93045
Madelief Hardeman <terra@proglab.nl>
2025-02-17 15:00:24 +0100

    Update Module_8/cypher.c

--- Module_8/cypher.c
+++ Module_8/cypher.c
@@ -29,147 +29,147 @@ void message_encrypter(string input)
     // loop through letters and encrypt 
     for (int letter = 0; letter < length; letter++)
     {
-        // if (islower(input[letter]))
-        // {
-        //     input[letter] = toupper(input[letter]);
-        // }
-        // else 
-        // {
-        //     input[letter] = tolower(input[letter]);
-        // }
+        if (islower(input[letter]))
+        {
+            input[letter] = toupper(input[letter]);
+        }
+        else 
+        {
+            input[letter] = tolower(input[letter]);
+        }
         
-        // // flip the alfabet :/
-        // if(input[letter] == 'a' || input[letter] == 'A')
-        // {
-        //     printf("%c", 'z');
-        // }
-        // else if (input[letter] == 'b' || input[letter] == 'B')
-        // {
-        //     printf("%c", 'y');
+        // flip the alfabet :/
+        if(input[letter] == 'a' || input[letter] == 'A')
+        {
+            printf("%c", 'z');
+        }
+        else if (input[letter] == 'b' || input[letter] == 'B')
+        {
+            printf("%c", 'y');
 
-        // }
-        // else if (input[letter] == 'c' || input[letter] == 'C')
-        // {
-        //     printf("%c", 'x');
+        }
+        else if (input[letter] == 'c' || input[letter] == 'C')
+        {
+            printf("%c", 'x');
 
-        // }
-        // else if (input[letter] == 'd' || input[letter] == 'D')
-        // {
-        //     printf("%c", 'w');
+        }
+        else if (input[letter] == 'd' || input[letter] == 'D')
+        {
+            printf("%c", 'w');
 
-        // }
-        // else if (input[letter] == 'e' || input[letter] == 'E')
-        // {
-        //     printf("%c", 'v');
+        }
+        else if (input[letter] == 'e' || input[letter] == 'E')
+        {
+            printf("%c", 'v');
 
-        // }
-        // else if (input[letter] == 'f' || input[letter] == 'F')
-        // {
-        //     printf("%c", 'u');
+        }
+        else if (input[letter] == 'f' || input[letter] == 'F')
+        {
+            printf("%c", 'u');
 
-        // }
-        // else if (input[letter] == 'g' || input[letter] == 'G')
-        // {
-        //     printf("%c", 't');
+        }
+        else if (input[letter] == 'g' || input[letter] == 'G')
+        {
+            printf("%c", 't');
 
-        // }
-        // else if (input[letter] == 'h' || input[letter] == 'H')
-        // {
-        //     printf("%c", 's');
+        }
+        else if (input[letter] == 'h' || input[letter] == 'H')
+        {
+            printf("%c", 's');
 
-        // }
-        // else if (input[letter] == 'i' || input[letter] == 'I')
-        // {
-        //     printf("%c", 'r');
+        }
+        else if (input[letter] == 'i' || input[letter] == 'I')
+        {
+            printf("%c", 'r');
 
-        // }
-        // else if (input[letter] == 'j' || input[letter] == 'J')
-        // {
-        //     printf("%c", 'q');
+        }
+        else if (input[letter] == 'j' || input[letter] == 'J')
+        {
+            printf("%c", 'q');
 
-        // }
-        // else if (input[letter] == 'k' || input[letter] == 'K')
-        // {
-        //     printf("%c", 'p');
+        }
+        else if (input[letter] == 'k' || input[letter] == 'K')
+        {
+            printf("%c", 'p');
 
-        // }
-        // else if (input[letter] == 'l' || input[letter] == 'L')
-        // {
-        //     printf("%c", 'o');
+        }
+        else if (input[letter] == 'l' || input[letter] == 'L')
+        {
+            printf("%c", 'o');
 
-        // }
-        // else if (input[letter] == 'm' || input[letter] == 'M')
-        // {
-        //     printf("%c", 'n');
+        }
+        else if (input[letter] == 'm' || input[letter] == 'M')
+        {
+            printf("%c", 'n');
 
-        // }else if (input[letter] == 'n' || input[letter] == 'N')
-        // {
-        //     printf("%c", 'm');
+        }else if (input[letter] == 'n' || input[letter] == 'N')
+        {
+            printf("%c", 'm');
 
-        // }
-        // else if (input[letter] == 'o' || input[letter] == 'O')
-        // {
-        //     printf("%c", 'l');
+        }
+        else if (input[letter] == 'o' || input[letter] == 'O')
+        {
+            printf("%c", 'l');
 
-        // }
-        // else if (input[letter] == 'p' || input[letter] == 'P')
-        // {
-        //     printf("%c", 'k');
+        }
+        else if (input[letter] == 'p' || input[letter] == 'P')
+        {
+            printf("%c", 'k');
 
-        // }
-        // else if (input[letter] == 'q' || input[letter] == 'Q')
-        // {
-        //     printf("%c", 'j');
+        }
+        else if (input[letter] == 'q' || input[letter] == 'Q')
+        {
+            printf("%c", 'j');
 
-        // }
-        // else if (input[letter] == 'r' || input[letter] == 'R')
-        // {
-        //     printf("%c", 'i');
+        }
+        else if (input[letter] == 'r' || input[letter] == 'R')
+        {
+            printf("%c", 'i');
 
-        // }
-        // else if (input[letter] == 's' || input[letter] == 'S')
-        // {
-        //     printf("%c", 'h');
+        }
+        else if (input[letter] == 's' || input[letter] == 'S')
+        {
+            printf("%c", 'h');
 
-        // }
-        // else if (input[letter] == 't' || input[letter] == 'T')
-        // {
-        //     printf("%c", 'g');
+        }
+        else if (input[letter] == 't' || input[letter] == 'T')
+        {
+            printf("%c", 'g');
 
-        // }
-        // else if (input[letter] == 'u' || input[letter] == 'U')
-        // {
-        //     printf("%c", 'f');
-        // }
-        // else if (input[letter] == 'v' || input[letter] == 'V')
-        // {
-        //     printf("%c", 'e');
+        }
+        else if (input[letter] == 'u' || input[letter] == 'U')
+        {
+            printf("%c", 'f');
+        }
+        else if (input[letter] == 'v' || input[letter] == 'V')
+        {
+            printf("%c", 'e');
 
-        // }
-        // else if (input[letter] == 'w' || input[letter] == 'W')
-        // {
-        //     printf("%c", 'd');
+        }
+        else if (input[letter] == 'w' || input[letter] == 'W')
+        {
+            printf("%c", 'd');
 
-        // }
-        // else if (input[letter] == 'x' || input[letter] == 'X')
-        // {
-        //     printf("%c", 'c');
+        }
+        else if (input[letter] == 'x' || input[letter] == 'X')
+        {
+            printf("%c", 'c');
 
-        // }
-        // else if (input[letter] == 'y' || input[letter] == 'Y')
-        // {
-        //     printf("%c", 'b');
+        }
+        else if (input[letter] == 'y' || input[letter] == 'Y')
+        {
+            printf("%c", 'b');
 
-        // }
-        // else if (input[letter] == 'z' || input[letter] == 'Z')
-        // {
-        //     printf("%c", 'a');
+        }
+        else if (input[letter] == 'z' || input[letter] == 'Z')
+        {
+            printf("%c", 'a');
 
-        // }
-        // else 
-        // {
-        //     printf("%c", input[letter]);
-        // }
+        }
+        else 
+        {
+            printf("%c", input[letter]);
+        }
     
     } 
 }

99338eb2862a3c61a728e07890f1e9384654bfa8
Madelief Hardeman <terra@proglab.nl>
2025-02-17 09:26:54 +0100

    Update Module_7/klinkers.c

--- Module_7/klinkers.c
+++ Module_7/klinkers.c
@@ -9,7 +9,9 @@ int main(void)
 {
     //ask input
     string woord_1 = get_string("Woord 1: ");
-    string woord_2 = get_string("Woord 2: ")
+    string woord_2 = get_string("Woord 2: ");
+    
+    //vowel count
 }
 
 //functions

597e028e49eb1285a9c8c9bc7917cb17e4c11323
Madelief Hardeman <terra@proglab.nl>
2025-02-17 13:03:24 +0100

    Update Module_7/wachtwoord.c

--- Module_7/wachtwoord.c
+++ Module_7/wachtwoord.c
@@ -68,7 +68,7 @@ bool two_char_in_row(string pasword, int length)
     }
     if (end_counter == 0);
     {
-        return  false; // false because no twotwo the same characters in a row were found
+        return  false; // false because no two the same characters in a row were found
     }
     }
 }

c54bdde595c10ef37f8021b86463795fa94e86cb
Madelief Hardeman <terra@proglab.nl>
2025-02-17 11:15:20 +0100

    Update Module_7/alfabet.c

--- Module_7/alfabet.c
+++ Module_7/alfabet.c
@@ -9,7 +9,7 @@ int main(void)
 {
     // ask input
     string woord_1 = get_string("Woord 1: ");
-    string woord_1 = get_string("Woord 2: ");
+    string woord_2 = get_string("Woord 2: ");
     
 }
 

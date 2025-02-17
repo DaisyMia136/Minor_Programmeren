fd1eb0807224895d10142a40efc3ecdd41593f39
Madelief Hardeman <terra@proglab.nl>
2025-02-17 15:52:47 +0100

    Update Module_8/cypher.c

--- Module_8/cypher.c
+++ Module_8/cypher.c
@@ -27,8 +27,8 @@ void message_encrypter(string input)
     int length = strlen(input);
     
     // setup array with the alphabet
-    char alfabet[26] = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z} 
-    printf("%c/n", alfabet[])
+    char alfabet[25] = { a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z} 
+    printf("%c/n", alfabet[0]);
     
     
     

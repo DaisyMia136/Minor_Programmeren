521ae1493db59c2d0dfa7e6938055037c80df759
Madelief Hardeman <114565182+DaisyMia136@users.noreply.github.com>
2025-02-17 16:38:24 +0100

    Update cypher.c

--- Module_8/cypher.c
+++ Module_8/cypher.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+\#include <stdio.h>
 #include <string.h>
 #include <cs50.h>
 #include <ctype.h>
@@ -15,29 +15,45 @@ int main(void)
     //encrypt message 
     message_encrypter(input);
     
-    return 0;
 }
 
 //functions
 void message_encrypter(string input)
 {
     // initial variables ++ DECLARATE THE VARIABLES :)
-    
+    int index = 0;
+    int counter = 2;
     // calc input length
     int length = strlen(input);
     
     // setup array with the alphabet
-    char alfabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char alfabet[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
 
-    for (int i = 26; i >=0; i--)
-    {
-        alfabet[26 + i] = toupper(alfabet[i]);
-        printf("%c", alfabet[26 + i]);
-    }
+    printf("cyphertext: ");
     
     for (int letter = 0; letter < length; letter++)
     {
-        strchr(alfabet, input[letter])
+        for (int i = 0; i <= 52; i++)
+        { 
+            if(input[letter] == alfabet[i])
+            {
+                index = i;
+        
+                if (isupper(input[letter]))
+                { 
+                    printf("%c", alfabet[index - 26]);
+                }
+                else
+                {
+                    printf("%c", alfabet[index + 26]);
+                }
+            } 
+            
+        }
+        if (isblank(input[letter]))
+            {
+                printf(" ");
+            }
     }
-    
-    
+    printf("\n");
+}

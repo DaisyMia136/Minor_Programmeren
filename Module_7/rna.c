6d46dc79183906e56bf6331c5f00dedf56c48a71
Madelief Hardeman <terra@proglab.nl>
2025-02-17 10:43:25 +0100

    Update Module_7/rna.c

--- Module_7/rna.c
+++ Module_7/rna.c
@@ -51,6 +51,7 @@ int DNA_input(string DNA)
         else if (DNA[nucleotide] != 'A' && DNA[nucleotide] != 'a' && DNA[nucleotide] != 'G' && DNA[nucleotide] != 'g' && DNA[nucleotide] != 'C' && DNA[nucleotide] != 'c' && DNA[nucleotide] != 'T' && DNA[nucleotide] != 't' && DNA[nucleotide] != 'U' && DNA[nucleotide] != 'u')
         {
             non_nucleotide_counter++;
+            printf("%i\n", non_nucleotide_counter);
             
         }
     }

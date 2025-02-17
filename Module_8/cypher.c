2868d603bfc2fe32e926c64b0ad8833cc9dfbaec
Madelief Hardeman <terra@proglab.nl>
2025-02-17 14:31:39 +0100

    Update Module_8/cypher.c

--- Module_8/cypher.c
+++ Module_8/cypher.c
@@ -30,7 +30,7 @@ void message_encrypter(string input)
     // loop through letters and encrypt 
     for (int letter = 0; letter < length; letter++)
     {
-        printf("%c")
+        printf("%c", input[letter] -1 )
     }
     
 }

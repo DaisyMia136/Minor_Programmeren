9ff5c338c356af94cd2906ff8bb4ee93cf79450b
Madelief Hardeman <terra@proglab.nl>
2025-02-17 11:34:10 +0100

    Update Module_7/alfabet.c

--- Module_7/alfabet.c
+++ Module_7/alfabet.c
@@ -62,8 +62,7 @@ void dictionary_check(string woord_1, string woord_2)
     }
     if (end_counter == 0)
     { 
-        printf("No need to decide!
-")
+        printf("No need to decide!");
     
     
     

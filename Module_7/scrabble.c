3f6f03a7f6c55be6e1d8aab2b2a38f2f928075c3
Madelief Hardeman <terra@proglab.nl>
2025-02-17 13:48:24 +0100

    Update Module_7/scrabble.c

--- Module_7/scrabble.c
+++ Module_7/scrabble.c
@@ -51,7 +51,6 @@ int calc_scrabble_points(string word)
     //loop through all letters
     for(int letter = 0; letter < length; letter++)
     { 
-        printf("%c\n", word[letter]);
         // convert all uppercase letters to lower case
         if (isupper(word[letter]))
         {

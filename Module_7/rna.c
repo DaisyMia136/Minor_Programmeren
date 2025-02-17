adb25a15490e4a68d039bcf48f20fda2d209e7f1
Madelief Hardeman <terra@proglab.nl>
2025-02-17 09:53:44 +0100

    Update Module_7/rna.c

--- Module_7/rna.c
+++ Module_7/rna.c
@@ -27,6 +27,6 @@ void print_RNA(string DNA)
     int DNA_length = strlen(DNA);
     
     // loop through DNA input
-    for(int nucleotide = 0; nucleotide <= )
+    for(int nucleotide = 0; nucleotide <= DNA_length; nucleotide)
     
 }

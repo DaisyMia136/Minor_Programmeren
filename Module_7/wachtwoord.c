6f1d697b6393ffd6fa4e0b2ab80477ccec5e3090
Madelief Hardeman <terra@proglab.nl>
2025-02-17 12:42:27 +0100

    Update Module_7/wachtwoord.c

--- Module_7/wachtwoord.c
+++ Module_7/wachtwoord.c
@@ -14,7 +14,7 @@ int main(void)
         string pasword = get_string(" Wachtwoord? ");
         bool strong = pasword_checker(pasword);
         if (strong)
-        
+        valid 
     }
     
     // pasword checker

bd95a6a5626ebc5555f216f1dd14046f1b192ddc
Madelief Hardeman <terra@proglab.nl>
2025-02-17 16:04:25 +0100

    Update Module_8/cypher.c

--- Module_8/cypher.c
+++ Module_8/cypher.c
@@ -29,9 +29,9 @@ void message_encrypter(string input)
     // setup array with the alphabet
     char alfabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    for (int i = 0; i <=26; i++)
+    for (int i = 26; i <=26; i--)
     {
-        alfabet[26 + i] = toupper(alfabet[i - 1]);
+        alfabet[26 + i] = toupper(alfabet[i]);
         printf("%c", alfabet[26 + i]);
     }
     

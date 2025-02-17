0267161dd08b34e77bc2a91f3399b35401c92698
Madelief Hardeman <terra@proglab.nl>
2025-02-17 09:38:33 +0100

    Update Module_7/klinkers.c

--- Module_7/klinkers.c
+++ Module_7/klinkers.c
@@ -24,12 +24,12 @@ int main(void)
     } 
     else if (vowel_2 < vowel_1)
     { 
-        printf("%s", vowel_1);
+        printf("%s", woord_1);
     }
     else
     {
-        printf("%s", vowel_1);
-        printf("%s", vowel_2);
+        printf("%s", woord_1);
+        printf("%s", woord_2);
     }
 }
 

49394197aa153ca8947aa18992f27ad7828d1b13
Madelief Hardeman <terra@proglab.nl>
2025-02-17 13:27:57 +0100

    Update Module_7/scrabble.c

--- Module_7/scrabble.c
+++ Module_7/scrabble.c
@@ -10,7 +10,9 @@ int main(void)
 {
     // ask input
     string word_1 = get_string("Player 1: ");
-    string word_2 = get_string("Player 2: ")
+    string word_2 = get_string("Player 2: ");
+    
+    // calculate points 
 }
 
 //functions

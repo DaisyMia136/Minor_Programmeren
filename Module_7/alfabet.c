11f9e07323965b4c769bbd0123bc57be31a83bd1
Madelief Hardeman <terra@proglab.nl>
2025-02-17 11:45:23 +0100

    Update Module_7/alfabet.c

--- Module_7/alfabet.c
+++ Module_7/alfabet.c
@@ -42,7 +42,7 @@ void dictionary_check(string woord_1, string woord_2)
     printf("Length %i\n", length);
     
     // loop through the shortest word
-    for (int letter = 0; letter <= length || end_counter != 0; letter++)
+    for (int letter = 0; letter <= length && end_counter != 0; letter++)
     {
         // convert all characters to lowercase
         woord_1[letter] = tolower(woord_1[letter]); 

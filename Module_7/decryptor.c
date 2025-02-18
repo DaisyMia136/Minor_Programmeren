e44757cc637c73bcb22c03cbbd81321cfe9647c6
Madelief Hardeman <terra@proglab.nl>
2025-02-18 08:31:52 +0100

    Update Module_7/decryptor.c

--- Module_7/decryptor.c
+++ Module_7/decryptor.c
@@ -19,7 +19,9 @@ int main(void)
 //functions
 void decryptor(string message)
 {
-    
+// set initial 
+{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+
 // check which letter is most common
     // message length
     int length = strlen(message);

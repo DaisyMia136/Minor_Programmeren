1d94d63b29df8352eca1e7f309ac97339be02651
Madelief Hardeman <terra@proglab.nl>
2025-02-18 12:22:48 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -114,7 +114,7 @@ void days_till_month(int month, int duration)
     // initial variables
     days = 0;
     
-    for (int months = 0; months <= month; months++)
+    for (int months = 0; months < month; months++)
     {
         days += duration[months];
     }

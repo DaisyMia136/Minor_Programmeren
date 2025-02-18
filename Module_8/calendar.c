51c97bcfded3658f62b80266a06d9db00df584c6
Madelief Hardeman <terra@proglab.nl>
2025-02-18 12:42:09 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -8,8 +8,8 @@ void display_calendar(int year, int month);
 void display_header(int year, int month);
 void display_grid(int year, int month, int duration);
 int first_day(int year, int month, int duration);
-int number_of_days_from_1800(int year, int month)
-int days_till_year(int year, int month)
+int number_of_days_from_1800(int year, int month);
+int days_till_year(int year, int month);
 void days_till_month(int year int month, int duration)
 bool check_leapyears(int years)
 void space_printer(int first_day)

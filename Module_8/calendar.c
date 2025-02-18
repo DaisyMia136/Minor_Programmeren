a5bbb3afd0c2e62d29ab3cfdcece139115b3995c
Madelief Hardeman <terra@proglab.nl>
2025-02-18 12:46:29 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -50,7 +50,7 @@ void display_header(int year, int month)
 }
 
 //print grit
-void display_grid(int year, int month, int duration)
+void display_grid(int year, int month)
 {
     string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}; 
     int duration[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 

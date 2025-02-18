894f396e1777fff8fbc7c7ed1a7e44e24d42d429
Madelief Hardeman <terra@proglab.nl>
2025-02-18 12:49:20 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -83,7 +83,9 @@ int number_of_days_from_1800(int year, int month)
     int day_year = days_till_year(year, month);
     
     // calc days between 1st jan and 1st of inputted month
-    int days_till_month(year, month, duration);
+    int day_month = days_till_month(year, month, duration);
+    days = day_year + day_month;
+    return days;
 }
 
 int days_till_year(int year, int month)

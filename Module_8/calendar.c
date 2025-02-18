06e3a37023cd584d8baad54f0fdefefb4e343ddb
Madelief Hardeman <terra@proglab.nl>
2025-02-18 12:21:21 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -111,7 +111,7 @@ int days_till_year(int year, int month)
 // days from 1 jan till 1 current month
 void days_till_month(int month, int duration)
 {
-    for (int months = 0; months < duration)
+    for (int months = 0; months < month )
 }
 
 

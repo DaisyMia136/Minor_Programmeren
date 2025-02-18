1e93cb9807e1c7fc5ee178f2cbee00a04285f8cf
Madelief Hardeman <terra@proglab.nl>
2025-02-18 11:03:16 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -89,7 +89,7 @@ int number_of_days_from_1800(int year, int month);
 int years_between_1800_and_current(int year, int month)
 {
     years_between = year - 1800; 
-    int check_leapyears(years_between);
+    int  check_leapyears(years_between);
 }
 
 // check leapyears

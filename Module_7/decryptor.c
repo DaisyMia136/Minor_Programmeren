681b3c1141370d86f567a994ae92de47fc2eaa84
Madelief Hardeman <terra@proglab.nl>
2025-02-18 08:34:30 +0100

    Update Module_7/decryptor.c

--- Module_7/decryptor.c
+++ Module_7/decryptor.c
@@ -28,7 +28,11 @@ char alfabet_lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
     
     for(int letter = 0; letter < length; letter++)
     {
-        if 
+        if (isupper(letter))
+        {
+            
+        }
+        
         for(int alfa = 0; alfa < 26; alfa++)
         {
             

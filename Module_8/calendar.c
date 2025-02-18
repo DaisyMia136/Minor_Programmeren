3c18c9282187dbb302a8bcf02b5fc2d83727c88e
Madelief Hardeman <terra@proglab.nl>
2025-02-18 11:09:08 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -91,7 +91,7 @@ int days_till_year(int year, int month)
     for(int years = 1800; years < year; years++)
     {
         // check if leap_year
-        
+        leap =  check_leapyears(year)
         
     }
     // count days 

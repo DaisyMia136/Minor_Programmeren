b23dba5204f9289e98fab075fb7a9296c71bea66
Madelief Hardeman <terra@proglab.nl>
2025-02-18 08:38:52 +0100

    Update Module_7/decryptor.c

--- Module_7/decryptor.c
+++ Module_7/decryptor.c
@@ -22,6 +22,7 @@ void decryptor(string message)
 // set initial 
 char alfabet_lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 char character = ' ';
+int index = 0;
 
 // check which letter is most common
     // message length

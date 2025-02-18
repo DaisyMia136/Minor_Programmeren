5430059e8c5f8956f5cc831aadad5f0a5f12f841
Madelief Hardeman <terra@proglab.nl>
2025-02-18 08:43:16 +0100

    Update Module_7/decryptor.c

--- Module_7/decryptor.c
+++ Module_7/decryptor.c
@@ -21,6 +21,7 @@ void decryptor(string message)
 {
 // set initial 
 char alfabet_lower[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+string alfabet_lower = 
 char character = ' ';
 int index = 0;
 

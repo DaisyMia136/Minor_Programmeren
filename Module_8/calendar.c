faf84a4ba6d5274b501c25ddd1c64bb600bbbe74
Madelief Hardeman <terra@proglab.nl>
2025-02-18 12:24:19 +0100

    Update Module_8/calendar.c

--- Module_8/calendar.c
+++ Module_8/calendar.c
@@ -81,7 +81,8 @@ int number_of_days_from_1800(int year, int month);
     int day_year = days_till_year(year, month);
     
     // calc days between 1st jan and 1st of inputted month
-    
+    void days_till_month(int month, int year, int duration)
+
     // check leap years
     
 }

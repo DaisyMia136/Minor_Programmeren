e7db47212705e934fb1f16d652585d5f8b743f91
Madelief Hardeman <terra@proglab.nl>
2025-02-24 12:57:18 +0100

    Update Module_10/helpers.c

--- Module_10/helpers.c
+++ Module_10/helpers.c
@@ -10,7 +10,7 @@ void sort(int values[], int n) //haystack and size
     // TODO: implement an O(n^2) sorting algorithm
     
     // loop through values (current value)
-    for(int current_value = 0; current_value < n; current_value++)
+    for(int current_value = 0; current_value < n - 1; current_value++)
     {
         // loop through subsequent values (current++)
         for (int lowest_value = current_value + 1; lowest_value < n)

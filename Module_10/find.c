506ee3dc1a9fc4e359892aadb1ccd894c86d09e1
Madelief Hardeman <terra@proglab.nl>
2025-02-24 14:30:06 +0100

    Update Module_10/find.c

--- Module_10/find.c
+++ Module_10/find.c
@@ -108,7 +108,7 @@ bool search(int value, int values[], int n)
     do
     {
         printf("n: %i\n", n/2);
-        //printf("values: %i\n", values[n/2]);
+        printf("values: %i\n", values[n/2]);
         
         if (values[(n/2)] == value)
         {

2a021b58693879c51fe6d390c47465eec4b292bd
Madelief Hardeman <terra@proglab.nl>
2025-02-24 13:58:24 +0100

    Update Module_10/find.c

--- Module_10/find.c
+++ Module_10/find.c
@@ -112,7 +112,7 @@ bool search(int value, int values[], int n)
     else if (value < values[n/2])
     {
         // search in lower half
-        values
+        values[]
     }
     else if (value > values[n/2])
     {

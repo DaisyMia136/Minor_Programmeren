03b72c933e680304e53aa84c36434adcec5382f7
Madelief Hardeman <terra@proglab.nl>
2025-02-24 15:22:21 +0100

    Update Module_10/find.c

--- Module_10/find.c
+++ Module_10/find.c
@@ -163,6 +163,6 @@ void sort(int values[], int n)
         //printf("%i\n", pre_fix_array[pre_fix_count]);
         pre_fix_array[pre_fix_count] = pre_fix_array[pre_fix_count - 1] + pre_fix_array[pre_fix_count];
     }
-    printf("%i\n", freq_array[10]);
+    printf("%i\n", pre_fix_array[10]);
     return;
 }

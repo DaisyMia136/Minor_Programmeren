4c6a026b8708e9114e1049ca661d3c98c6b215a3
Madelief Hardeman <terra@proglab.nl>
2025-02-24 13:02:16 +0100

    Update Module_10/sort.c

--- Module_10/sort.c
+++ Module_10/sort.c
@@ -64,5 +64,18 @@ void generate_random_numbers(int arr[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+    // loop through values (current value)
+    for(int current_value = 0; current_value < n - 1; current_value++)
+    {
+        // loop through subsequent values (current++)
+        for (int lowest_value = current_value + 1; lowest_value < n; lowest_value++)
+        {
+            //find lowest value in subsequent values
+            
+            
+        }
+        // replace current value with lowest value
+        values[current_value] = values[lowest_value];
+    }
     return;
 }

1929dc09e1ee5a4858e73410149107e1a3beb19d
Madelief Hardeman <terra@proglab.nl>
2025-02-24 12:57:31 +0100

    Update Module_10/helpers.c

--- Module_10/helpers.c
+++ Module_10/helpers.c
@@ -15,7 +15,7 @@ void sort(int values[], int n) //haystack and size
         // loop through subsequent values (current++)
         for (int lowest_value = current_value + 1; lowest_value < n; lowest_value++)
         {
-            //find lowest value in subsequent values 
+            //find lowest value in subsequent values
             
         // replace current value with lowest value
         values[current_value] = values[lowest_value];

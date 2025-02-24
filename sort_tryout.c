3144d9223d9fdc85e36e434d3d28dc771636bcba
Madelief Hardeman <terra@proglab.nl>
2025-02-24 13:33:49 +0100

    Update sort_tryout.c

--- sort_tryout.c
+++ sort_tryout.c
@@ -27,7 +27,7 @@ void sort(int values[], int n)
 {
     int new_value = 0;
     // loop through values (current value)
-    for(int replace_value = 0; replace_value < n - 1; replace_value++)
+    for(int replace_value = 0; replace_value <= n - 1; replace_value++)
     {
     //printf("replace counter: %i\n", replace_value);
 

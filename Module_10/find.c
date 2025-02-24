c0c2e8fd58556d5df277f3813fb9210b78e37575
Madelief Hardeman <terra@proglab.nl>
2025-02-24 15:52:55 +0100

    Update Module_10/find.c

--- Module_10/find.c
+++ Module_10/find.c
@@ -186,10 +186,10 @@ void sort(int values[], int n)
         }
     }
     
-    // for (int j = 0; j < n; j++)
-    // {
-    //     printf("%i ", sorted_array[j]);
-    // }
+    for (int j = 0; j < n; j++)
+    {
+        printf("%i ", sorted_array[j]);
+    }
 
     
     

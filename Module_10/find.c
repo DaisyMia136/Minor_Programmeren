e118525a4b5946007bf551bb1192bb16ea998365
Madelief Hardeman <terra@proglab.nl>
2025-02-24 15:06:41 +0100

    Update Module_10/find.c

--- Module_10/find.c
+++ Module_10/find.c
@@ -139,7 +139,7 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement the sorting algorithm
-    freq_array[66536];
+    int freq_array[66536];
     
     for(int number = 0; number < n; number++)
     {

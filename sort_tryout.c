a15bf545659fb9970101c20d4ea56219a6b5fe50
Madelief Hardeman <terra@proglab.nl>
2025-02-24 13:23:08 +0100

    Update sort_tryout.c

--- sort_tryout.c
+++ sort_tryout.c
@@ -19,47 +19,8 @@ void sort(int values[], int n);
 
 int main(int argc, string argv[])
 {
-    // Ensure proper usage
-    if (argc != 2)
-    {
-        printf("Usage: ./sort <n>\n");
-        return 1;
-    }
-
-    int size = atoi(argv[1]);
-    if (size <= 1 || size > MAX)
-    {
-        printf("Please enter a valid size between 1 and %d\n", MAX);
-        return 1;
-    }
-
-    // seed the random number generator
-    srand48((long int) time(NULL));
-
-    // generate random numbers
-    int haystack[size];
-    generate_random_numbers(haystack, size);
-
-    // sort the array
-    sort(haystack, size);
-
-    // print the sorted array
-    for (int i = 0; i < size; i++)
-    {
-        printf("%i\n", haystack[i]);
-    }
+    
 }
-
-// Fills array of size n with random numbers
-void generate_random_numbers(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        // random number between 0 and LIMIT
-        arr[i] = (int) (drand48() * LIMIT);
-    }
-}
-
 // Sorts array of n values
 void sort(int values[], int n)
 {

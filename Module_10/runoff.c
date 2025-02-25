dda5bca633d0a18b85f0aec3c3bbdbe076e2379c
Madelief Hardeman <terra@proglab.nl>
2025-02-25 11:00:33 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -126,7 +126,7 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    int counter = 0;
     // check if name is a part of candidates.names
     for (int check = 0; check < argc; check++)
     {

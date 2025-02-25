eca4e0a7575f88267298956c43877b4347cbed56
Madelief Hardeman <terra@proglab.nl>
2025-02-25 12:16:21 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -129,7 +129,7 @@ bool vote(int voter, int rank, string name)
     
     //preferences[voter][rank] = name;
     printf("%i", preferences[voter][rank]);
-    return true;
+    return false;
 }
 
 // Tabulate votes for non-eliminated candidates

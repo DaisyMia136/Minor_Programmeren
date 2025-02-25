d50e3f4e7f1934a60b65d8005d75234480603c5c
Madelief Hardeman <terra@proglab.nl>
2025-02-25 11:05:38 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     // check if name is a part of candidates.names
     for (int check = 0; check < 9; check++)
     {
-        if (name = candidates[check].name)
+        if (name == candidates[check].name)
         {
             counter++;
         }

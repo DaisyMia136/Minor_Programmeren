e40890b5a09e5687e519940656ac1fb6eab29d89
Madelief Hardeman <terra@proglab.nl>
2025-02-25 12:27:41 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -127,7 +127,7 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     
-    preferences[voter][rank] = name ;
+    preferences[voter][rank] = ;
     printf("%i", preferences[voter][rank]);
     
     return true;

a12e5d28bcbc877d67cd37185a656a30ee9d6c72
Madelief Hardeman <terra@proglab.nl>
2025-02-27 11:31:50 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -181,7 +181,7 @@ bool print_winner(void)
     
     // calc votes needed for more than 50%
     int votes_50 = round(voter_count / 2); 
-    printf("%i", votes_50);
+    //printf("%i", votes_50);
     printf("%i", candidates[1].votes);
     
     

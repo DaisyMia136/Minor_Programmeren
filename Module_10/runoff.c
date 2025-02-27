23d516392e9df11313982fc82cc20c10cc82774c
Madelief Hardeman <terra@proglab.nl>
2025-02-27 14:13:49 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -164,7 +164,6 @@ void tabulate(void)
     for(int voter = 0; voter < voter_count; voter++)
     {
         index = preferences[voter][counter_eliminated];
-        
         if (candidates[index].eliminated == false)
         {
             //printf("voter %i: %i", voter, index);

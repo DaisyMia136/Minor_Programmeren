0a08694d0e98303fe503be53f5b37385787f3de7
Madelief Hardeman <terra@proglab.nl>
2025-02-27 13:28:41 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -221,7 +221,7 @@ int find_min(void)
                     smallest_counter++;
                 }
             }
-        if (smallest_counter == )
+        if (smallest_counter == candidate_count)
             
         }    
     } 

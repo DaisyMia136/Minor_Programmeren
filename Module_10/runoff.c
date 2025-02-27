84c239c8dd7b0ea33e34bf4c9ee055ed8ea089a2
Madelief Hardeman <terra@proglab.nl>
2025-02-27 11:04:06 +0100

    Update Module_10/runoff.c

--- Module_10/runoff.c
+++ Module_10/runoff.c
@@ -160,7 +160,7 @@ void tabulate(void)
     / TODO
     for(int voter = 0; voter < voter_count; voter++)
     {
-        int index = preferences[voter][0];
+        index = preferences[voter][0];
         printf("%i", index);
         candidates[index].votes++;
         //printf("%i", candidates[index].votes);

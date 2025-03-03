82f0efef4a3e3b3230c57944fc7fd77873e8aa0c
Madelief Hardeman <terra@proglab.nl>
2025-03-03 13:08:59 +0100

    Update Module_11/snowfall.c

--- Module_11/snowfall.c
+++ Module_11/snowfall.c
@@ -34,9 +34,9 @@ int main(void)
         // update scene 
         
             
-            move_snowflack();    
+            move_snowflake();    
             
-            // add snowflack to bottom
+            // add snowflake to bottom
         
 }
 

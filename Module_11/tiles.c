6fd89d119916c380ee90660492be1a197215ac63
Madelief Hardeman <terra@proglab.nl>
2025-03-03 10:48:43 +0100

    Update Module_11/tiles.c

--- Module_11/tiles.c
+++ Module_11/tiles.c
@@ -264,7 +264,7 @@ bool move(int tile)
                 //printf("%i \n %i \n\n", tile_x, tile_y);
                 break;
             }
-            else (board[heigth][length] == tile)
+            else (board[heigth][length] == '_')
             {
                 
             }

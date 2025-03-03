61013bc9a6a1e380a0d25766bc89979463981a12
Madelief Hardeman <terra@proglab.nl>
2025-03-03 12:15:22 +0100

    Update Module_11/snowfall.c

--- Module_11/snowfall.c
+++ Module_11/snowfall.c
@@ -47,7 +47,8 @@ void move_snowflack(void)
 // generate random snowflack location > generate random x coordinate 
     // use the current time to pick a seed for the random number generator
     srandom(time(NULL));
-    int x_snowflack = random() / ((double) RAND_MAX + 1) * EDGE_MAX;
+    // generate a random number between 0 and 80
+    int x_snowflack = random() / ((double) RAND_MAX + 1) * EDGE_MAX; 
     printf("%i", x_snowflack);
             
             // print snowflack * 

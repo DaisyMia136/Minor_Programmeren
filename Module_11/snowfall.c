968f75a8d62ae5f4058aa9e044cdb368df65f46e
Madelief Hardeman <terra@proglab.nl>
2025-03-03 12:19:51 +0100

    Update Module_11/snowfall.c

--- Module_11/snowfall.c
+++ Module_11/snowfall.c
@@ -56,7 +56,7 @@ void move_snowflack(void)
     for (int y_snowflake = Y_MAX; y_snowflake > 0; y_snowflake--)
     {
         printf("\033[%d;%dH*", y_snowflake, x_snowflake);
-        printf()
+        printf("\033[%d;%dH")
     }
             // move snowflack down    
 }

247d991b82f9f6f27eac0f3751e4783e9c7045ae
Madelief Hardeman <terra@proglab.nl>
2025-03-03 13:11:20 +0100

    Update Module_11/snowfall.c

--- Module_11/snowfall.c
+++ Module_11/snowfall.c
@@ -70,7 +70,8 @@ void move_snowflake(void)
     srandom(time(NULL));
     // generate a random number between 0 and 80
     int x_snowflake = random() / ((double) RAND_MAX + 1) * X_MAX; 
-    //printf("%i", x_snowflake);
+    
+    
             
 // print snowflake * 
             // move snowflake down    

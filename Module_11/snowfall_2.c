74b1a1e9d69135d3b0e0e481ae3c978cd54f1f2d
Madelief Hardeman <terra@proglab.nl>
2025-03-03 15:00:18 +0100

    Update Module_11/snowfall_2.c

--- Module_11/snowfall_2.c
+++ Module_11/snowfall_2.c
@@ -82,7 +82,10 @@ void update_scene(void)
             {
                  scene[y][x] = 1;
             }
-            if(scene[y + 1][x] == 1 && )
+            if(scene[y][x] == 1 && scene[y + 1][x])
+            {
+                scene[y][x] = 1;
+            }
             
             if (scene[y][x] == 1)
             {

d63715d0c3b0cddb08ec56ce23f20efebee49312
Madelief Hardeman <terra@proglab.nl>
2025-03-03 14:05:58 +0100

    Update Module_11/snowfall_2.c

--- Module_11/snowfall_2.c
+++ Module_11/snowfall_2.c
@@ -99,7 +99,7 @@ void print_scene(void)
         for (int y = 0; y < Y_MAX; y++)
         {
             print_snowflake(scene[x][y]);
-            terra_sleep(50);
+            //terra_sleep(50);
         }
         printf("\n");
     }

bbb29248d22b9b14929a3caf7694f0179c9d996a
Madelief Hardeman <terra@proglab.nl>
2025-03-03 15:16:10 +0100

    Update Module_11/snowfall_2.c

--- Module_11/snowfall_2.c
+++ Module_11/snowfall_2.c
@@ -162,6 +162,6 @@ void add_snowflake(void)
     int x = random() / ((double) RAND_MAX + 1) * X_MAX; // generate a random number between 0 and MAX
     
     // add y_snowflake to the scene
-    scene[0][x] = 1;
+    scene[][x] = 1;
     //printf("new snowflake added");
 }

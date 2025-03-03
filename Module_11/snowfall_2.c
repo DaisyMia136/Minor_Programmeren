d1595ab73e390bb324e8ecb7023f756e0594ba8a
Madelief Hardeman <terra@proglab.nl>
2025-03-03 14:31:27 +0100

    Update Module_11/snowfall_2.c

--- Module_11/snowfall_2.c
+++ Module_11/snowfall_2.c
@@ -5,8 +5,8 @@
 #include <time.h>
 
 // global variables
-    #define X_MAX 20
-    #define Y_MAX 20
+    #define X_MAX 40
+    #define Y_MAX 40
     #define new_snow 5
     #define COLOR "\033[32m"
     int timestep = 0;

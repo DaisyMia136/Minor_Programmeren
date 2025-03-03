fdbcf905dfd0bdf7051a713638c52ba97f4abe66
Madelief Hardeman <terra@proglab.nl>
2025-03-03 11:02:04 +0100

    Update Module_11/tiles.c

--- Module_11/tiles.c
+++ Module_11/tiles.c
@@ -279,11 +279,10 @@ bool move(int tile)
     // check if _ is adjecent to tile
     if (_x == tile_x + 1 || _x == tile_x - 1 || _y == tile_y + 1 || _y == tile_y - 1)
     {
-        // set the '_' to the value of the inputted tile
-        board[_x][_y] = tile;
-        
         // set the inputted tile to 0, representing '_'
         board[tile_x][tile_y] = 0;
+        // set the '_' to the value of the inputted tile
+        board[_x][_y] = tile;
         
         return true;
     }

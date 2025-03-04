6bc642de858789853ff44820aed295400d690661
Madelief Hardeman <terra@proglab.nl>
2025-03-04 12:47:00 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -71,7 +71,7 @@ void print_board(void)
 {
     for (int y = 0; y > Y_MAX; y++)
     {
-        for (int x = 0; x < X_MAX; x++)
+        for (int x = 0; x > X_MAX; x++)
         {
             printf("%c", board[y][x]);
         }

77cd14b864d545d25a500a1cafac6e735d2ba48a
Madelief Hardeman <terra@proglab.nl>
2025-03-04 11:11:33 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -81,7 +81,8 @@ void update_board(void)
         for (int x = 0; x < X_MAX; x++)
         {
             if (board[y][x] == '#' && 
-            (board[y - 1][x] == '#' || board[y + 1][x] == '#' || board[y][x - 1] == '#' || board[y][x + 1] == '#' || board[y - 1][x - 1] == '#' || board[y - 1][x + 1] == '#' || board[y + 1][x - 1] == '#' || board[y + 1][x + 1] == '#')
+            (board[y - 1][x] == '#' || board[y + 1][x] == '#' || board[y][x - 1] == '#' || board[y][x + 1] == '#' || 
+            board[y - 1][x - 1] == '#' || board[y - 1][x + 1] == '#' || board[y + 1][x - 1] == '#' || board[y + 1][x + 1] == '#')
             {
                 
             }

d3258fff68c797e493f305079dabc9ee91190858
Madelief Hardeman <terra@proglab.nl>
2025-03-04 11:31:09 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -93,7 +93,7 @@ void update_board(void)
         for (int x = 0; x < X_MAX; x++)
         {
             // define pixels around the current pixel 
-            
+            adjacent_pixel(board[])
             
             // loop through pixels around it
             for (int around_y = y - 1; around_y <= y + 1; y++)

a6a05ca2f76a2db903926ad54f5e5db3465735b1
Madelief Hardeman <terra@proglab.nl>
2025-03-07 14:42:48 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -180,8 +180,14 @@ void update_board(void)
             // do changes
             board[y][x] = log_1[y][x];
             // clear log
-            if ()
-            log_1[y][x] = ' ';
+            if ((y == 0 || y == Y_MAX - 1) && (x == 0 || x == X_MAX - 1))
+            {
+                board[y][x] = '0';
+            }
+            else 
+            {
+                log_1[y][x] = ' ';
+            }
         }
     }
     int adjacent_counter = 0;

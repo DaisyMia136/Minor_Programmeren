fdb7a8b2ed1807c683b36dc615d29aac622ca827
Madelief Hardeman <terra@proglab.nl>
2025-03-07 14:49:54 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -37,7 +37,7 @@ int main(void)
         fflush(stdout);
         
         //sleep 
-        terra_sleep(2000);
+        terra_sleep(1000);
         
         // clear screen
         clear_screen();

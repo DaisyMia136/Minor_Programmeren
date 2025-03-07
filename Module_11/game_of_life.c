e2cc986d9d2c538a207983f43e9973e1a7b20730
Madelief Hardeman <terra@proglab.nl>
2025-03-07 12:32:02 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -143,7 +143,7 @@ int adjacent_pixels(int y, int x)
     {
         for (int around_x = x - 1; around_x <= x + 1; around_x++)
         {
-            y = 1;
+            y = ;
             x = 2;
             if (board[around_y][around_x] == '#' || board[around_y][around_x] == '*')
             {

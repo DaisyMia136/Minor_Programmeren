e6674b406e44f1f221a3b7ed2483f2d840629d43
Madelief Hardeman <terra@proglab.nl>
2025-03-07 13:26:37 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -28,7 +28,7 @@ int main(void)
     // initiat board
     init_board();
     
-    // print board
+    print board
     //print_board();
     
     //terra_sleep(20);
@@ -37,7 +37,7 @@ int main(void)
     //while (true)
     //{
         //update board
-        update_board();
+        //update_board();
         // clear screen
         //clear_screen();
         // print board 

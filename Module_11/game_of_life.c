e04fd390397de32c3e3f6dc802db54793de30a3d
Madelief Hardeman <terra@proglab.nl>
2025-03-07 11:59:44 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -91,7 +91,7 @@ void clear_screen(void)
 
 void update_board(void)
 {
-    printf("\n\n ik ben hier ");
+    printf("\n\n ik ben hier \n");
     // log changes 
     for (int y = 0; y < Y_MAX; y++)
     {

98b6969f4d21bcf465e0f2410a64a7dbc05348ff
Madelief Hardeman <terra@proglab.nl>
2025-03-07 13:33:34 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -75,11 +75,11 @@ void init_board(void)
     // board[3][2] = '#';
     // board[2][1] = '#';
     // board[2][3] = '#';
-    // board[14][55] = '#';
-    // board[15][55] = '#';
-    // board[16][55] = '#';
-    // board[15][54] = '#';
-    // board[15][56] = '#';
+    board[14][55] = '#';
+    board[15][55] = '#';
+    board[16][55] = '#';
+    board[15][54] = '#';
+    board[15][56] = '#';
 }
 
 void print_board(void)

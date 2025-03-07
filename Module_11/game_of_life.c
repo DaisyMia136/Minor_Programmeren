277bf197cac0a6d1d5bfccac88412e793a05ae7a
Madelief Hardeman <terra@proglab.nl>
2025-03-07 12:15:13 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -139,7 +139,7 @@ int adjacent_pixels(int y, int x)
 {
     int adjacent_counter = 0;
 // loop through pixels around it
-    for (int around_y = y - 1; around_y < y + 1; around_y++)
+    for (int around_y = y - 1; around_y <= y + 1; around_y++)
     {
         for (int around_x = x - 1; around_x < x + 1; around_x++)
         {

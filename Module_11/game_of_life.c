36f977eeecbe11987a316bb68e8aa3d734b28620
Madelief Hardeman <terra@proglab.nl>
2025-03-07 12:28:40 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -59,11 +59,11 @@ void init_board(void)
     }
     
     // initiate figure a cross in the middle
-    board[3][2] = '#';
-    board[3][3] = '#';
-    board[3][4] = '#';
     board[2][3] = '#';
+    board[3][3] = '#';
     board[4][3] = '#';
+    board[3][2] = '#';
+    board[3][4] = '#';
     // board[14][55] = '#';
     // board[15][55] = '#';
     // board[16][55] = '#';

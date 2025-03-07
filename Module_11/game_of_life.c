15b297802aacde108b527ed61b4cf6e83b6aa854
Madelief Hardeman <terra@proglab.nl>
2025-03-07 11:53:23 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -19,7 +19,7 @@ void terra_sleep(int milliseconds);
 #define Y_MAX 20
 #define COLOR "\033[32m"
 static char board[Y_MAX][X_MAX];
-static char log[Y_MAX][X_MAX];
+static char log_1[Y_MAX][X_MAX];
 
 
 // main script

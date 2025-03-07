c98a6b26164916c5989d4cfaebc6c9c722f34137
Madelief Hardeman <terra@proglab.nl>
2025-03-07 13:35:44 +0100

    Update Module_11/game_of_life.c

--- Module_11/game_of_life.c
+++ Module_11/game_of_life.c
@@ -111,7 +111,8 @@ void update_board(void)
         {
             // define pixels around the current pixel 
             int adjacent_counter = adjacent_pixels(y, x);
-            if (board[y][x] == '')
+            if (board[y][x] == '#')
+            {
             
             //define if current pixel is dead or alive
             if (adjacent_counter == 0 || adjacent_counter == 1 || adjacent_counter >= 4)

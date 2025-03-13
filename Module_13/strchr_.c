7b521763f6a10c77b0544ce0ff38d280faa1c4c6
Madelief Hardeman <terra@proglab.nl>
2025-03-13 14:25:58 +0100

    Update Module_13/strchr_.c

--- Module_13/strchr_.c
+++ Module_13/strchr_.c
@@ -22,5 +22,5 @@ int main(int argc, char *argv[])
 char * strchr_(char *word, char letter)
 {
     // loop through word and find the first instance of letter
-    for (int i = 0, n = strlen(word); i < )
+    for (int i = 0, n = strlen(word); i < n; )
 }

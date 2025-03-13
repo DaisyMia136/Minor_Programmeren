5f8f3123f46bfae2f076c24edb531202bb478317
Madelief Hardeman <terra@proglab.nl>
2025-03-13 15:18:47 +0100

    Update Module_13/strchr_.c

--- Module_13/strchr_.c
+++ Module_13/strchr_.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
         printf("not enough input arguments");
     }
     
-    letter = 
+    int letter = argv[2];
     //printf("%i", letter);
     
     char *pointer = strchr_(argv[1], 98);

ee8dc781e1598710fe1d030d339d0e2fb9b515f6
Madelief Hardeman <terra@proglab.nl>
2025-03-13 13:39:07 +0100

    Update Module_13/strfry_.c

--- Module_13/strfry_.c
+++ Module_13/strfry_.c
@@ -43,7 +43,7 @@ void strfry(char *string, int length)
         index_random = (random() / ((double) RAND_MAX + 1)) * length;
 
         // check if it is present in the array (while)
-        for(int j = 0; j < length; j++)
+        for(int j = 0; j <= length; j++)
         {
             if (index_random == random_array[j]) // if present generate a new number + start again
             {

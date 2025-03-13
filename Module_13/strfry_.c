5672664dca478677ce437931acec124bb18438ac
Madelief Hardeman <terra@proglab.nl>
2025-03-13 13:39:29 +0100

    Update Module_13/strfry_.c

--- Module_13/strfry_.c
+++ Module_13/strfry_.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     // calc string length
     int length = strlen(argv[1]);
     char *word = argv[1];
-    //printf("%s", word);
+    printf("%s", word);
 
     strfry(word, length);
 

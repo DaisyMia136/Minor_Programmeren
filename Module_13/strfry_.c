2369f3d2b5e66c24418028bbe34d18034718c429
Madelief Hardeman <terra@proglab.nl>
2025-03-13 13:37:55 +0100

    Update Module_13/strfry_.c

--- Module_13/strfry_.c
+++ Module_13/strfry_.c
@@ -31,7 +31,7 @@ void strfry(char *string, int length)
     // randomised seed
     srandom(time(NULL));
 
-    printf("ik ben hier"); // %i", length);
+   
     // initialise array with length of string
     int random_array[length];
     int index_random = 0;

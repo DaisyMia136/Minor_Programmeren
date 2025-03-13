ab2f2f5d9100c1913a62dfdf00f66a545d727f1b
Madelief Hardeman <terra@proglab.nl>
2025-03-13 14:41:46 +0100

    Update Module_13/strchr_.c

--- Module_13/strchr_.c
+++ Module_13/strchr_.c
@@ -49,6 +49,6 @@ char * strchr_(char *word, char *letter)
         return NULL;
     }
     // return pointer to first instance of letter
-    return *word[0 + index_letter];
+    return &word[0 + index_letter];
     
 }

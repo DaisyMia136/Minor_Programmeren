8389edded045723c8bca6ec87ea475f75c76112d
Madelief Hardeman <terra@proglab.nl>
2025-03-13 14:42:02 +0100

    Update Module_13/strchr_.c

--- Module_13/strchr_.c
+++ Module_13/strchr_.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     }
     
     // 
-    char *pointer = strchr_(argv[1], argv[2]);
+    char *letter = strchr_(argv[1], argv[2]);
     
     if (pointer == NULL)
     {

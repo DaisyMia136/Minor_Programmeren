c3215da2d99e5e0ea7e4a91b91d12a88c16d7ee0
Madelief Hardeman <terra@proglab.nl>
2025-03-13 13:58:08 +0100

    Update Module_13/strfry_.c

--- Module_13/strfry_.c
+++ Module_13/strfry_.c
@@ -57,12 +57,12 @@ void strfry_(char *string, int length)
                     j = 0;
                 }
                 else if (index_random == random_array[j]) // if present generate a new number + start again
-            {
-                printf("found a double: %i\n", index_random);
-                index_random = (random() / ((double) RAND_MAX + 1)) * length;
-                //printf("%i", index_random);
-                j = 0;
-            }
+                {
+                    printf("found a double: %i\n", index_random);
+                    index_random = (random() / ((double) RAND_MAX + 1)) * length;
+                    //printf("%i", index_random);
+                    j = 0;
+                }
                 
             }
             else if (index_random == random_array[j]) // if present generate a new number + start again

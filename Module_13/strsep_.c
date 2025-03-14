2141fd8721ac8ce2bba3ca2e3e52f63448db200b
Madelief Hardeman <terra@proglab.nl>
2025-03-14 13:03:46 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -23,6 +23,6 @@ char *strsep_(char *string, char *delimiter)
     // calc strlen of string
     
     // loop through string until delimiter is found
-        // when delimiter is found 
+        // when delimiter is found make new pointer to 
     
 }

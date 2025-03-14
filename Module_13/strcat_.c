166ba5a6b750a4fdbc2b8f807cbae3f9be0a21c5
Madelief Hardeman <terra@proglab.nl>
2025-03-14 11:05:05 +0100

    Update Module_13/strcat_.c

--- Module_13/strcat_.c
+++ Module_13/strcat_.c
@@ -29,7 +29,7 @@ char * strcat_(char * string_1, char *string_2)
     char output_string[(lenght_1 + lenght_2 - 1)];
     for (int j = 0; j < lenght_1 + lenght_2 - 2; j++)
     {
-        output_string[j] = ' ';
+        output_string[j] = '*';
     }
     output_string[lenght_1 + lenght_2 - 1] = '\0';
     

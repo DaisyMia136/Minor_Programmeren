dc6d3c43b371c4b1065df060cd83ce9fc0547c76
Madelief Hardeman <terra@proglab.nl>
2025-03-14 11:10:59 +0100

    Update Module_13/strcat_.c

--- Module_13/strcat_.c
+++ Module_13/strcat_.c
@@ -48,7 +48,7 @@ char * strcat_(char * string_1, char *string_2)
         output_string[i] = string_1[i];
         i++;
     }
-
+    printf("%c", string_2[0]);
     
     for (int letter = 0; letter < lenght_2; letter++ && i++)
     {

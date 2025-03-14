2e4fba18dffb343ec278056a5a07c8fc856f1f19
Madelief Hardeman <terra@proglab.nl>
2025-03-14 11:04:52 +0100

    Update Module_13/strcat_.c

--- Module_13/strcat_.c
+++ Module_13/strcat_.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
         printf("not enough input argumnets");
     }
     
-    combined_string = strcat_(argv[1], argv[2])
+    combined_string = strcat_(argv[1], argv[2]);
     
 }
 

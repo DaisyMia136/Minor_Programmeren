be090ea6cf553335604f8fa26dd87ad76c12a539
Madelief Hardeman <terra@proglab.nl>
2025-03-14 11:29:19 +0100

    Update Module_13/strcat_.c

--- Module_13/strcat_.c
+++ Module_13/strcat_.c
@@ -49,7 +49,7 @@ char * strcat_(char * string_1, char *string_2)
         string_1[j] = '*';
     }
     string_1[lenght_1 + lenght_2 + 1] = '\0';
-    
+    printf("%s\n", string_1);
     printf("%s\n", string_2);
     // loop through string_1 until \0 is found
     int i = 0;

bd37333f8b993452ab3cdb31b695d834396b21e4
Madelief Hardeman <terra@proglab.nl>
2025-03-15 09:46:59 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -42,7 +42,7 @@ char *strsep_(char *string, char *delimiter)
         if (strcmp(segment, compare) == 0)
         {
             string[i] = '\0';
-            return string[i + length_delim];
+            return &string[i + length_delim];
         }
     }
     // return old pointer 

09e89642ae4590b8c5f01fc9e3a7acc42598bd10
Madelief Hardeman <terra@proglab.nl>
2025-03-15 09:55:14 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -35,7 +35,6 @@ char *strsep_(char *string, char *delimiter)
         for (int j = 0; j < length_delim; j++)
         {
             compare[j] = string[i + j];
-            segment[j] = string[i + j];
             //printf("%c\n", compare[j]);
         }
         printf("%s\n", segment);

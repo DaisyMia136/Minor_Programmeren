8ec4fbba9d78d4e3de0d2d54be25f559d1dea390
Madelief Hardeman <terra@proglab.nl>
2025-03-15 10:05:30 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -33,7 +33,7 @@ char *strsep_(char *string, char *delimiter)
     {
         for (int j = 0; j < length_delim; j++)
         // when delimiter is found change current pointer to delimiter pointer + 1
-        if (
+        if (string[i] == 
             )
         {
             string[i] = '\0';

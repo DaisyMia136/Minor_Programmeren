3a14e66ed712c4f088a8d4478f2f8089b6829f49
Madelief Hardeman <terra@proglab.nl>
2025-03-15 09:39:14 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -33,7 +33,7 @@ char *strsep_(char *string, char *delimiter)
         {
             compare[j] = string[i + j];
         }
-        printf("%s\n", compare)
+        printf("%s\n", compare);
         // when delimiter is found change current pointer to delimiter pointer + 1
         if (strcmp(string[i], delimiter[0]) == 0)
         {

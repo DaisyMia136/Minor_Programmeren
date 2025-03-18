b7bbe917aba47790ad26b52d20f62668287a20aa
Madelief Hardeman <terra@proglab.nl>
2025-03-18 10:41:22 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -13,9 +13,8 @@ int main(int argc, char *argv[])
     {
         printf("not enough input arguments");
     }
-    char *string = argv[1];
     
-    argv[1] = strsep_(*string, argv[2]);
+    argv[1] = strsep_(argv[1], argv[2]);
     printf("%s", argv[1]);
 }
 

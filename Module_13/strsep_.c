8f080628ce2ad3b0a2a93fa74deedf2f29156286
Madelief Hardeman <terra@proglab.nl>
2025-03-18 10:12:48 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -51,7 +51,7 @@ char *strsep_(char *string, char *delimiter)
     if(end_string > 0)
     {
         // return pointer to new pointer 
-        return &string[end_string];
+        return &string[new_string];
     }
     else 
     {

972f9c254cd7e48fa3b6156f3db739a4d1c1c2e1
Madelief Hardeman <terra@proglab.nl>
2025-03-18 10:09:20 +0100

    Update Module_13/strsep_.c

--- Module_13/strsep_.c
+++ Module_13/strsep_.c
@@ -41,6 +41,7 @@ char *strsep_(char *string, char *delimiter)
             if (string[i] == delimiter[j])
             {
                 string[i] = '\0';
+                
                 // return pointer to new pointer 
                 return &string[i + 1];
             }

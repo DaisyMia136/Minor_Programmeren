a97b7ba9bcfc6947c35482f00892521ff14d9429
Madelief Hardeman <terra@proglab.nl>
2025-03-24 16:06:48 +0100

    Update Module_14/Linked.c

--- Module_14/Linked.c
+++ Module_14/Linked.c
@@ -65,7 +65,7 @@ int number = 0;
         // find last content of list 
         int j = 1;
         node *temp = head; 
-        while (j != i)
+        while (j != i + 1)
         {
             //printf("%i", j);
             temp = temp->next;
